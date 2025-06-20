0067cdfa95a322c0ecbba10a5c27018de4e855ea
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-06-20 16:49:31 -0500

    Digital Signage (#684)

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -39,6 +39,8 @@ import StaffBar from "./pages/makerspace_page/StaffBar";
 import { isAdmin, isManagerFor, isStaffFor } from "./common/PrivilegeUtils";
 import NoPrivilegePage from "./pages/NoPrivilegePage";
 import AnnouncementsDisplay from "./pages/signage/AnnouncementsDisplay";
+import HoursDisplay from "./pages/signage/HoursDisplay";
+import EventsDisplay from "./pages/signage/EventsDisplay";
 
 // This is where we map the browser's URL to a
 // React component with the help of React Router.
@@ -95,6 +97,8 @@ export default function AppRoutes() {
         {/* Routes for the sattic displays around the makerspaces */}
         <Route path="/display">
           <Route path="/display/announcements" element={<AnnouncementsDisplay/>}/>
+          <Route path="/display/hours/:makerspaceID" element={<HoursDisplay/>}/>
+          <Route path="/display/events" element={<EventsDisplay/>}/>
         </Route>
         {/* END STATIC DISPLAYS */}
 
