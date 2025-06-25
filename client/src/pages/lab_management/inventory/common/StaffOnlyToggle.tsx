e69b01fc48685f6498a4ac0f6d7a61765bab0d34
evstod <e.d.stoddard345@gmail.com>
2025-06-25 13:25:38 -0400

    Reformat materials page to use datagrids

---
+++ client/src/pages/lab_management/inventory/common/StaffOnlyToggle.tsx
@@ -0,0 +1,23 @@
+import { Switch } from "@mui/material";
+import InventoryItem from "../../../../types/InventoryItem";
+import { useMutation } from "@apollo/client";
+import { isManager } from "../../../../common/PrivilegeUtils";
+import { SET_STAFF_ONLY, GET_INVENTORY_ITEMS } from "../../../../queries/inventoryQueries";
+import { useCurrentUser } from "../../../../common/CurrentUserProvider";
+
+export function StaffOnlyToggle(props: { item: InventoryItem }) {
+  const currentUser = useCurrentUser();
+
+  const [setStaffOnly, setStaffOnlyMutation] = useMutation(SET_STAFF_ONLY, {
+    variables: { id: props.item.id, staffOnly: !props.item.staffOnly },
+    refetchQueries: [{ query: GET_INVENTORY_ITEMS }]
+  });
+
+  function handleToggleStaffOnly() {
+    setStaffOnly();
+  }
+
+  return (
+    <Switch onChange={handleToggleStaffOnly} disabled={!isManager(currentUser)} defaultChecked={props.item.staffOnly}></Switch>
+  );
+}
