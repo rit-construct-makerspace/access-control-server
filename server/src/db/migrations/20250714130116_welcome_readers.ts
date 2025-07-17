bc9b6271385ced874b93a837372e0e70bda52d59
cowsed <richie.sommers@outlook.com>
2025-07-17 11:14:16 -0400

    better migration already happened checks

--- server/src/db/migrations/20250714130116_welcome_readers.ts
+++ server/src/db/migrations/20250714130116_welcome_readers.ts
@@ -3,34 +3,37 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     const tableThere = await knex.schema.hasTable("MakerspaceWelcomeReaders");
-    if (tableThere) {
+    if (!tableThere) {
+        await knex.schema.createTable("MakerspaceWelcomeReaders", (t) => {
+            t.bigInteger("makerspaceID").notNullable();
+            t.foreign("makerspaceID").references("id").inTable("Zones");
+            t.bigInteger("readerID").notNullable();
+            t.foreign("readerID").references("id").inTable("Readers");
+
+            t.primary(["makerspaceID", "readerID"])
+        });
         return;
     }
 
-    await knex.schema.alterTable("Readers", (t)=>{
-        t.string("targetFirmwareVersion").nullable()
-    })
-    await knex.schema.createTable("MakerspaceWelcomeReaders", (t) => {
-        t.bigInteger("makerspaceID").notNullable();
-        t.foreign("makerspaceID").references("id").inTable("Zones");
-        t.bigInteger("readerID").notNullable();
-        t.foreign("readerID").references("id").inTable("Readers");
-
-        t.primary(["makerspaceID", "readerID"])
-    });
+    if (!(await knex.schema.hasColumn("Readers", "targetFirmwareVersion"))) {
+        await knex.schema.alterTable("Readers", (t) => {
+            t.string("targetFirmwareVersion").nullable()
+        })
+    }
 }
 
 
 export async function down(knex: Knex): Promise<void> {
     const tableThere = await knex.schema.hasTable("MakerspaceWelcomeReaders");
-    if (!tableThere) {
-        return;
+    if (tableThere) {
+        await knex.schema.dropTable("MakerspaceWelcomeReaders");
     }
-    await knex.schema.alterTable("Readers", (t)=>{
-        t.dropColumn("targetFirmwareVersion");
-    })
+    if (await knex.schema.hasColumn("Readers", "targetFirmwareVersion")) {
+        await knex.schema.alterTable("Readers", (t) => {
+            t.dropColumn("targetFirmwareVersion");
+        })
+    }
 
-    await knex.schema.dropTable("MakerspaceWelcomeReaders");
 
 }
 
