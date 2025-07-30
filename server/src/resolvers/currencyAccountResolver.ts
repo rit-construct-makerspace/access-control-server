70bce44015dcacca405870edd44dfcd1cf07ff7e
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-07-30 16:58:51 -0400

    Removed incorrect resolver endpoints

--- server/src/resolvers/currencyAccountResolver.ts
+++ server/src/resolvers/currencyAccountResolver.ts
@@ -17,30 +17,4 @@ export const CurrencyAccountResolvers = {
             return isStaff(async () => (await CurrencyAccountRepo.getAccountByID(args.accountID)));
         },
     },
-
-    Mutation: {
-        adjustAccountBalanceCents: async (
-            _parent: any,
-            args: {
-                accountID: number,
-                amount: number,
-                description: string
-            },
-            { isManager }: ApolloContext
-        ) => {
-            return isManager(async () => (await CurrencyAccountRepo.adjustAccountBalanceCents(args.accountID, args.amount, "Make", args.description)));
-        },
-
-        adjustAccountBalanceIfAvailableCents: async (
-            _parent: any,
-            args: {
-                accountID: number,
-                amount: number,
-                description: string
-            },
-            { isManager }: ApolloContext
-        ) => {
-            return isManager(async () => (CurrencyAccountRepo.adjustAccountBalanceIfAvailableCents(args.accountID, args.amount, "Make", args.description)));
-        },
-    }
 }
