6c13f2eaaed8e3c30d36cc0f1ed203763d5f58d5
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-08-01 13:48:39 -0400

    Made searchable accounts

--- client/src/pages/lab_management/currency/CurrencyAccounts.tsx
+++ client/src/pages/lab_management/currency/CurrencyAccounts.tsx
@@ -1,11 +1,112 @@
-import { Grid } from "@mui/material";
+import { Button, Card, Grid, Stack, Typography } from "@mui/material";
+import gql from "graphql-tag";
+import RequestWrapper2 from "../../../common/RequestWrapper2";
+import { useLazyQuery, useQuery } from "@apollo/client";
+import SearchBar from "../../../common/SearchBar";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
+const GET_CURRENCY_ACCOUNTS_LIMIT = gql`
+  query CurrencyAccountsLimit($searchText: String) {
+    currencyAccountsLimit(searchText: $searchText) {
+      id
+      balance
+      owner {
+        username
+        displayName
+        userID
+        orgID
+      }
+    }
+  }
+`;
 
-export default function CurrencyAccouns() {
+type CurrencyAccountOwner = {
+  username: string;
+  displayName: string;
+  userID: number | null;
+  orgID: number | null;
+}
+
+type CurrencyAccount = {
+  id: number;
+  balance: number;
+  owner: CurrencyAccountOwner;
+}
+
+export default function CurrencyAccounts() {
+  const { makerspaceID } = useParams<{ makerspaceID: string }>();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [getCurrencyAccounts, currencyAccountsResult] = useLazyQuery(GET_CURRENCY_ACCOUNTS_LIMIT);
+
+  const [searchText, setSearchText] = useState("");
+
+  const setUrlParam = (paramName: string, paramValue: string) => {
+    const params = new URLSearchParams(location.search);
+    params.set(paramName, paramValue);
+    navigate(`/makerspace/${makerspaceID}/currency?` + params, { replace: true });
+  };
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const queryString = searchParams.get("q") ?? "";
+
+    setSearchText(queryString);
+
+    getCurrencyAccounts({
+      variables: {
+        searchText: queryString,
+      },
+    });
+  }, [location.search, getCurrencyAccounts]);
+
+  const moneyForamtter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  })
 
   return (
-    <Grid>
+    <RequestWrapper2 result={currencyAccountsResult} render={(data) => {
 
-    </Grid>
+      const accounts: CurrencyAccount[] = data.currencyAccountsLimit;
+
+      return (
+        <Stack spacing={2}>
+          <SearchBar
+            placeholder="Search Accounts"
+            sx={{ maxWidth: 300 }}
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            onClear={() => setUrlParam("q", "")}
+            onSubmit={() => setUrlParam("q", searchText)}
+          />
+          <Grid container spacing={2} justifyContent={"center"}>
+            {
+              accounts.map((account: CurrencyAccount) => {
+
+                return (
+                  <Grid>
+                    <Card variant="outlined" sx={{ height: "100%" }}>
+                      <Stack padding={"10px"} width={"350px"} height={"100%"} justifyContent={"space-between"} alignItems={"center"}>
+                        <Typography color="primary" fontWeight={"bold"}>{account.owner.displayName} ({account.owner.username})</Typography>
+                        <Stack direction={"row"} justifyContent={"space-around"} alignItems={"center"} width={"100%"}>
+                          <Typography><b>Account ID:</b> {account.id}</Typography>
+                          <Typography><b>Credits:</b> {moneyForamtter.format(account.balance)}</Typography>
+                        </Stack>
+                        <Button color="secondary" sx={{ alignSelf: "flex-end" }}>
+                          Manage
+                        </Button>
+                      </Stack>
+                    </Card>
+                  </Grid>
+                );
+              })
+            }
+          </Grid>
+        </Stack>
+      );
+    }} />
   );
 }
--- client/src/pages/lab_management/currency/CurrencyPage.tsx
+++ client/src/pages/lab_management/currency/CurrencyPage.tsx
@@ -1,12 +1,14 @@
 import { Stack, Typography } from "@mui/material";
+import CurrencyAccounts from "./CurrencyAccounts";
 
 
 export default function CurrencyPage() {
 
   return (
-    <Stack>
+    <Stack padding={"0 10px"} spacing={2}>
+      <title>Currency | Make @ RIT</title>
       <Typography variant="h5">Currency Accounts</Typography>
-
+      <CurrencyAccounts />
       <Typography variant="h5">Currency Ledger</Typography>
     </Stack>
   );
