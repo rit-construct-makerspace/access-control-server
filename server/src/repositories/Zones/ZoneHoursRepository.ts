fe7f707757988ae0b16ac1578aa2e56502e58c13
Richard Sommers <44383226+cowsed@users.noreply.github.com>
2025-08-01 09:09:02 -0400

    Node Package Updates and Security Fixes (#750)
    Apollo server to v5
    Express to v5
    Removed a bunch of unused dependencies (what was google api doing in there)
    Removed dependencies for nonexistent test framework
    Updated Passport SAML to mitigate bypassing SAML
    maybe some other things
    maybe broke shibbing due to breaking changes in Passport SAML

--- server/src/repositories/Zones/ZoneHoursRepository.ts
+++ server/src/repositories/Zones/ZoneHoursRepository.ts
@@ -9,7 +9,6 @@ import { DefaultHoursRow, SpecialHoursRow } from "../../db/tables.js";
 
 export async function getZoneHoursOnDay(day: Date, makerspaceID: number): Promise<SpecialHoursRow> {
     const special = await knex("SpecialHours").where({ makerspaceID: makerspaceID }).andWhereRaw(`CAST(day as DATE) = CAST('${day.toISOString()}' as DATE)`).select("*");
-    console.log(special);
     if (special.length > 0) {
         return special[0];
     }
