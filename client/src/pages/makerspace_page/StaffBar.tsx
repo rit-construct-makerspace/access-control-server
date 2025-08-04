5f9666f14f0488906c55cdf68b6d463b9fdc4e20
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-08-04 15:13:55 -0400

    Fixed spacing on staff bar

--- client/src/pages/makerspace_page/StaffBar.tsx
+++ client/src/pages/makerspace_page/StaffBar.tsx
@@ -18,137 +18,137 @@ import { Outlet, useParams } from "react-router-dom";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export default function StaffBar() {
-    const { makerspaceID } = useParams<{ makerspaceID: string }>();
+  const { makerspaceID } = useParams<{ makerspaceID: string }>();
 
-    const user = useCurrentUser();
-    const isMobile = useIsMobile();
-    const isPriviledged = isStaffFor(user, Number(makerspaceID));
-    const isTrainer = isOnlyTrainer(user);
+  const user = useCurrentUser();
+  const isMobile = useIsMobile();
+  const isPriviledged = isStaffFor(user, Number(makerspaceID));
+  const isTrainer = isOnlyTrainer(user);
 
-    const [mobileMenu, setMobileMenu] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
 
-    if (!isPriviledged && !isTrainer) {
-        return null;
-    }
+  if (!isPriviledged && !isTrainer) {
+    return null;
+  }
 
-    const staffNavigation = isTrainer
-        ? (
-            <Stack
-                direction={isMobile ? "column" : "row"}
-                justifyContent={isMobile ? "flex-start" : "space-around"}
-                alignItems="center"
-                padding="10px 0px"
-            >
-                {
-                    mobileMenu
-                        ? <ButtonBase onClick={() => setMobileMenu(false)} sx={{ width: "100%", padding: "10px 0px" }}>
-                            <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
-                                <Typography variant="body1" color="grey">Trainer Actions</Typography>
-                                <KeyboardArrowDownIcon />
-                            </Stack>
-                        </ButtonBase>
-                        : null
-                }
-                {
-                    window.location.pathname.match(/\/app\/makerspace\/\d+\/.+/gm) !== null
-                        ? <NavLink
-                            primary={"Back"}
-                            to={`/makerspace/${makerspaceID}`}
-                            icon={<ArrowBackIcon />}
-                        />
-                        : null
-                }
-                <NavLink
-                    primary={"People"}
-                    to={`/makerspace/${makerspaceID}/people`}
-                    icon={<PeopleIcon />}
-                />
-            </Stack>
-        )
-        : (
-            <Stack
-                direction={isMobile ? "column" : "row"}
-                justifyContent={isMobile ? "flex-start" : "space-around"}
-                alignItems="center"
-                padding="10px 0px"
-            >
-                {
-                    mobileMenu
-                        ? <ButtonBase onClick={() => setMobileMenu(false)} sx={{ width: "100%", padding: "10px 0px" }}>
-                            <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
-                                <Typography variant="body1" color="grey">Staff Actions</Typography>
-                                <KeyboardArrowDownIcon />
-                            </Stack>
-                        </ButtonBase>
-                        : null
-                }
-                {
-                    window.location.pathname.match(/\/app\/makerspace\/\d+\/.+/gm) !== null
-                        ? <NavLink
-                            primary={"Back"}
-                            to={`/makerspace/${makerspaceID}`}
-                            icon={<ArrowBackIcon />}
-                        />
-                        : null
-                }
-                <NavLink
-                    primary={"Manage Trainings"}
-                    to={`/makerspace/${makerspaceID}/trainings`}
-                    icon={<SchoolIcon />}
-                />
-                <NavLink
-                    primary={"Materials"}
-                    to={"/admin/inventory"}
-                    icon={<InventoryIcon />}
-                />
-                <NavLink
-                    primary={"Tools"}
-                    to={`/makerspace/${makerspaceID}/tools`}
-                    icon={<ArchitectureIcon />}
-                />
-                <NavLink
-                    primary={"Storefront"}
-                    to={"/admin/storefront"}
-                    icon={<StorefrontIcon />}
-                />
-                <NavLink
-                    primary={"People"}
-                    to={`/makerspace/${makerspaceID}/people`}
-                    icon={<PeopleIcon />}
-                />
-                <NavLink
-                    primary={"History"}
-                    to={`/makerspace/${makerspaceID}/history`}
-                    icon={<HistoryIcon />}
-                />
-                <NavLink
-                    primary={"Readers"}
-                    to={`/makerspace/${makerspaceID}/readers`}
-                    icon={<ScannerIcon />}
-                />
-                <NavLink
-                    primary={"Statistics"}
-                    to={"/admin/statistics"}
-                    icon={<BarChartIcon />}
-                />
-            </Stack>
-        );
-
-    return (
-        <Stack>
-            {
-                isMobile
-                    ? mobileMenu
-                        ? staffNavigation
-                        : <ButtonBase onClick={() => setMobileMenu(true)} sx={{ width: "100%", padding: "10px 0px" }}>
-                            <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
-                                <Typography variant="body1" color="grey">{isTrainer ? "Trainer Actions" : "Staff Actions"}</Typography>
-                                <MenuIcon />
-                            </Stack>
-                        </ButtonBase>
-                    : staffNavigation
-            }
-            <Outlet />
-        </Stack>
+  const staffNavigation = isTrainer
+    ? (
+      <Stack
+        direction={isMobile ? "column" : "row"}
+        justifyContent={isMobile ? "flex-start" : "space-around"}
+        alignItems="center"
+        padding="10px 0px"
+      >
+        {
+          mobileMenu
+            ? <ButtonBase onClick={() => setMobileMenu(false)} sx={{ width: "100%", padding: "10px 0px" }}>
+              <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
+                <Typography variant="body1" color="grey">Trainer Actions</Typography>
+                <KeyboardArrowDownIcon />
+              </Stack>
+            </ButtonBase>
+            : null
+        }
+        {
+          window.location.pathname.match(/\/app\/makerspace\/\d+\/.+/gm) !== null
+            ? <NavLink
+              primary={"Back"}
+              to={`/makerspace/${makerspaceID}`}
+              icon={<ArrowBackIcon />}
+            />
+            : null
+        }
+        <NavLink
+          primary={"People"}
+          to={`/makerspace/${makerspaceID}/people`}
+          icon={<PeopleIcon />}
+        />
+      </Stack>
+    )
+    : (
+      <Stack
+        direction={isMobile ? "column" : "row"}
+        justifyContent={isMobile ? "flex-start" : "space-around"}
+        alignItems="center"
+        padding="10px 0px"
+      >
+        {
+          mobileMenu
+            ? <ButtonBase onClick={() => setMobileMenu(false)} sx={{ width: "100%", padding: "10px 0px" }}>
+              <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
+                <Typography variant="body1" color="grey">Staff Actions</Typography>
+                <KeyboardArrowDownIcon />
+              </Stack>
+            </ButtonBase>
+            : null
+        }
+        {
+          window.location.pathname.match(/\/app\/makerspace\/\d+\/.+/gm) !== null
+            ? <NavLink
+              primary={"Back"}
+              to={`/makerspace/${makerspaceID}`}
+              icon={<ArrowBackIcon />}
+            />
+            : null
+        }
+        <NavLink
+          primary={"Manage Trainings"}
+          to={`/makerspace/${makerspaceID}/trainings`}
+          icon={<SchoolIcon />}
+        />
+        <NavLink
+          primary={"Materials"}
+          to={"/admin/inventory"}
+          icon={<InventoryIcon />}
+        />
+        <NavLink
+          primary={"Tools"}
+          to={`/makerspace/${makerspaceID}/tools`}
+          icon={<ArchitectureIcon />}
+        />
+        <NavLink
+          primary={"Storefront"}
+          to={"/admin/storefront"}
+          icon={<StorefrontIcon />}
+        />
+        <NavLink
+          primary={"People"}
+          to={`/makerspace/${makerspaceID}/people`}
+          icon={<PeopleIcon />}
+        />
+        <NavLink
+          primary={"History"}
+          to={`/makerspace/${makerspaceID}/history`}
+          icon={<HistoryIcon />}
+        />
+        <NavLink
+          primary={"Readers"}
+          to={`/makerspace/${makerspaceID}/readers`}
+          icon={<ScannerIcon />}
+        />
+        <NavLink
+          primary={"Statistics"}
+          to={"/admin/statistics"}
+          icon={<BarChartIcon />}
+        />
+      </Stack>
     );
+
+  return (
+    <Stack>
+      {
+        isMobile
+          ? mobileMenu
+            ? staffNavigation
+            : <ButtonBase onClick={() => setMobileMenu(true)} sx={{ width: "100%", padding: "10px 0px" }}>
+              <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%">
+                <Typography variant="body1" color="grey">{isTrainer ? "Trainer Actions" : "Staff Actions"}</Typography>
+                <MenuIcon />
+              </Stack>
+            </ButtonBase>
+          : staffNavigation
+      }
+      <Outlet />
+    </Stack>
+  );
 }
