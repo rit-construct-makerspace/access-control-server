ee7fd2a810b8fd846b1fa8bceb0a1424a7ad37d1
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-08-05 12:26:06 -0400

    Implemented migration

--- server/src/db/migrations/20250805162118_archive-alumni.ts
+++ server/src/db/migrations/20250805162118_archive-alumni.ts
@@ -2,9 +2,25 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+  const colsExist = await knex.schema.hasColumn("Users", "forceArchive");
+  if (colsExist) {
+    return;
+  }
+
+  await knex.schema.alterTable("Users", (t) => {
+    t.boolean("forceArchive").nullable().defaultTo(null);
+  })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+  const colsExist = await knex.schema.hasColumn("Users", "forceArchive");
+  if (!colsExist) {
+    return
+  }
+
+  await knex.schema.alterTable("Users", (t) => {
+    t.dropColumn("forceArchive");
+  })
 }
 
