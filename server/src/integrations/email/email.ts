9674ed91bd775387564896ad141a90cd7277c6cc
cowsed <richie.sommers@outlook.com>
2025-08-08 11:12:09 -0400

    dont send emails in dev

--- server/src/integrations/email/email.ts
+++ server/src/integrations/email/email.ts
@@ -2,7 +2,7 @@ import FormData from "form-data";
 import * as Mailgun from "mailgun.js"
 import { generateReceiptEmail } from "./receipt-template.js"
 import { Transaction } from "../currency/currency.js";
-import {generateExpiryEmail, ExpiryDescription} from "./training-expiry-template.js"
+import { generateExpiryEmail, ExpiryDescription } from "./training-expiry-template.js"
 const mailgun = new Mailgun.default(FormData);
 const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere' });
 const MAIL_DOMAIN = process.env.MAIL_DOMAIN ?? "";
@@ -15,15 +15,19 @@ type MessageSendResult = {
     details?: string;
 }
 export function send_generic_email(args: { fromAccount: string, to: string[], subject: string, htmlContent: string, textContent: string }): Promise<MessageSendResult> {
-    return mg.messages.create(MAIL_DOMAIN, {
-        from: `make@rit.edu <${args.fromAccount}@${MAIL_DOMAIN}>`,
-        bcc: ((process.env.NODE_ENV !== "development") ? ['make@rit.edu'] : []),
-        to: args.to,
-        subject: args.subject,
-        text: args.textContent,
-        html: args.htmlContent,
-        "h:Reply-To": 'make@rit.edu'
-    });
+    if (process.env.NODE_ENV !== 'development') {
+        return mg.messages.create(MAIL_DOMAIN, {
+            from: `make@rit.edu <${args.fromAccount}@${MAIL_DOMAIN}>`,
+            bcc: ((process.env.NODE_ENV !== "development") ? ['make@rit.edu'] : []),
+            to: args.to,
+            subject: args.subject,
+            text: args.textContent,
+            html: args.htmlContent,
+            "h:Reply-To": 'make@rit.edu'
+        });
+    } else {
+        console.log("NOT SENDING EMAIL BC DEV ENVIRONMENT");
+    }
 }
 
 const OVERRIDE_RECEIPT_EMAIL = process.env.OVERRIDE_RECEIPT_EMAIL;
@@ -31,7 +35,7 @@ const OVERRIDE_RECEIPT_EMAIL = process.env.OVERRIDE_RECEIPT_EMAIL;
 export async function send_transaction_email(emailAddress: string, transaction: Transaction) {
     const content = generateReceiptEmail(transaction);
 
-    if (OVERRIDE_RECEIPT_EMAIL){
+    if (OVERRIDE_RECEIPT_EMAIL) {
         emailAddress = OVERRIDE_RECEIPT_EMAIL;
     }
     await send_generic_email({
@@ -40,17 +44,17 @@ export async function send_transaction_email(emailAddress: string, transaction: 
         subject: `RIT SHED Receipt - ${transaction.date.toLocaleString()}`,
         textContent: content.text,
         htmlContent: content.html
-    }).catch((err: any) => {console.error("Error sending receipt email", err)});
+    }).catch((err: any) => { console.error("Error sending receipt email", err) });
 }
 
 
 
-export async function send_training_expiry_email(email: string, desc: ExpiryDescription){
+export async function send_training_expiry_email(email: string, desc: ExpiryDescription) {
     const content = generateExpiryEmail(desc);
     send_generic_email({
         fromAccount: "training",
-        to: ['res3453@rit.edu'],
-        subject: "RIT SHED: "+((desc.type == "warning") ? "Trainings Expiring Soon" : "Training Expiry Notice")+ " - " + new Date().toLocaleDateString(),
+        to: [email],
+        subject: "RIT SHED: " + ((desc.type == "warning") ? "Trainings Expiring Soon" : "Training Expiry Notice") + " - " + new Date().toLocaleDateString(),
         textContent: content.text,
         htmlContent: content.html,
     })
