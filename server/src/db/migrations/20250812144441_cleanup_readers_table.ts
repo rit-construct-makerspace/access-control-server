c72325d7d8c8594f2b3708ec525ee4033ff255e9
cowsed <richie.sommers@outlook.com>
2025-08-12 10:53:32 -0400

    migration the columns away

---
+++ server/src/db/migrations/20250812144441_cleanup_readers_table.ts
@@ -0,0 +1,26 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    if (! await knex.schema.hasColumn("Readers", "machineType")) {
+        return;
+    }
+
+    await knex.schema.alterTable("Readers", (t)=>{
+        t.dropColumns("machineType", "machineID", "zone");
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    if (await knex.schema.hasColumn("Readers", "machineType")) {
+        return;
+    }
+
+    await knex.schema.alterTable("Readers", (t)=>{
+        t.integer("machineID").references("id").inTable("Equipment").nullable();
+        t.string("machineType");
+        t.string("zone");
+    })
+}
+
