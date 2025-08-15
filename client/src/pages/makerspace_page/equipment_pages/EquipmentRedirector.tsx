eca270257b00fdde8002fe16c27adbdcd9703860
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-08-15 09:03:24 -0400

    Created Equipment Redirecter

---
+++ client/src/pages/makerspace_page/equipment_pages/EquipmentRedirector.tsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import { useNavigate, useParams } from "react-router-dom";
+import RequestWrapper2 from "../../../common/RequestWrapper2";
+import { Button } from "@mui/material";
+
+const GET_EQUIPMENT_MAKERSPACE = gql`
+  query GetEquipmentMakerspace($id: ID!) {
+    equipment(id: $id) {
+      room {
+        zone {
+          id
+        }
+      }
+    }
+  }
+`;
+
+export default function EquipmentRedirector() {
+  const { equipmentID } = useParams<{ equipmentID: string }>();
+
+  const navigate = useNavigate();
+
+  const getEquipmentMakerspaceResult = useQuery(GET_EQUIPMENT_MAKERSPACE, { variables: { id: equipmentID } });
+
+  return (
+    <RequestWrapper2 result={getEquipmentMakerspaceResult} render={(data) => {
+      navigate(`/makerspace/${data.equipment.room.zone.id}`);
+      return (
+        <Button variant="contained" onClick={() => navigate("/")}>You shouldn't be here</Button>
+      );
+    }} />
+  );
+}
