fa79a75da8b27c6e5d723323c1411d675fbbe185
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2025-08-15 01:15:24 -0400

    formatter

--- client/src/common/EquipmentCard.tsx
+++ client/src/common/EquipmentCard.tsx
@@ -10,122 +10,122 @@ import ReactMarkdown from "react-markdown";
 import ModuleStatusRow from "./ModuleStatusRow";
 
 interface EquipmentCardProps {
-    equipment: Equipment;
-    isMobile: boolean;
-    staffMode: boolean;
+  equipment: Equipment;
+  isMobile: boolean;
+  staffMode: boolean;
 }
 
 export default function EquipmentCard(props: EquipmentCardProps) {
-    const user = useCurrentUser();
-    const navigate = useNavigate();
-    const theme = useTheme();
-    const isPriviledged = props.staffMode;
-    const hasApprovedAccessCheck: boolean = user.accessChecks.some((ac) => Number(ac.equipmentID) === Number(props.equipment.id) && ac.approved)
+  const user = useCurrentUser();
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const isPriviledged = props.staffMode;
+  const hasApprovedAccessCheck: boolean = user.accessChecks.some((ac) => Number(ac.equipmentID) === Number(props.equipment.id) && ac.approved)
 
-    const moduleStatuses = props.equipment.trainingModules.map(
-        moduleStatusMapper(user.passedModules, user.trainingHolds)
-    );
+  const moduleStatuses = props.equipment.trainingModules.map(
+    moduleStatusMapper(user.passedModules, user.trainingHolds)
+  );
 
-    return (
-        <Card sx={{
-            width: props.isMobile ? "350px" : "600px",
-            backgroundColor: props.equipment.archived ? theme.palette.error.light : undefined,
-            height: "100%"
-        }}>
-            <CardContent sx={{ width: "100%", height: "100%" }}>
-                <Stack height={"100%"}>
-                    <Stack direction="row" height="200px">
-                        {props.isMobile ? null :
-                            <Stack alignItems="center">
-                                <Box width="150px" height="175px">
-                                    <CardMedia
-                                        component="img"
-                                        image={(props.equipment.imageUrl === undefined || props.equipment.imageUrl == null || props.equipment.imageUrl === "") ? import.meta.env.BASE_URL + "/shed_acronym_vert.jpg" : "" + import.meta.env.VITE_CDN_URL + import.meta.env.VITE_CDN_EQUIPMENT_DIR + "/" + props.equipment.imageUrl}
-                                        alt={`Picture of ${props.equipment.name}`}
-                                        sx={{ width: "150px", height: "175px", backgroundColor: "lightgray" }}
-                                    />
-                                </Box>
-                                {isPriviledged ? <Typography variant="body2">ID {props.equipment.id}</Typography> : null}
-                            </Stack>
+  return (
+    <Card sx={{
+      width: props.isMobile ? "350px" : "600px",
+      backgroundColor: props.equipment.archived ? theme.palette.error.light : undefined,
+      height: "100%"
+    }}>
+      <CardContent sx={{ width: "100%", height: "100%" }}>
+        <Stack height={"100%"}>
+          <Stack direction="row" height="200px">
+            {props.isMobile ? null :
+              <Stack alignItems="center">
+                <Box width="150px" height="175px">
+                  <CardMedia
+                    component="img"
+                    image={(props.equipment.imageUrl === undefined || props.equipment.imageUrl == null || props.equipment.imageUrl === "") ? import.meta.env.BASE_URL + "/shed_acronym_vert.jpg" : "" + import.meta.env.VITE_CDN_URL + import.meta.env.VITE_CDN_EQUIPMENT_DIR + "/" + props.equipment.imageUrl}
+                    alt={`Picture of ${props.equipment.name}`}
+                    sx={{ width: "150px", height: "175px", backgroundColor: "lightgray" }}
+                  />
+                </Box>
+                {isPriviledged ? <Typography variant="body2">ID {props.equipment.id}</Typography> : null}
+              </Stack>
+            }
+
+            <Stack height="100%" width={"100%"}>
+              {/* Title & Edit button */}
+              <Stack direction="row" justifyContent="space-between" pl={"10px"}>
+                <Typography variant="h6">{props.equipment.archived ? `${props.equipment.name} (Hidden)` : props.equipment.name}</Typography>
+                {
+                  isPriviledged
+                    ? <Button
+                      onClick={() => { navigate(`/admin/equipment/${props.equipment.archived ? "archived/" : ""}${props.equipment.id}`) }}
+                      aria-label="edit button"
+                      sx={{ width: "40px", height: "40px" }}
+                      variant="contained"
+                      color="primary"
+                    >
+                      <ConstructionIcon />
+                    </Button>
+                    : null
+                }
+              </Stack>
+              <Stack direction="row" justifyContent="space-between" height="100%">
+                {/* Trainings & Access Check */}
+                <Stack width="100%">
+                  {moduleStatuses.map((ms: ModuleStatus) => (
+                    <ModuleStatusRow ms={ms} />
+                  ))}
+                  {
+                    !props.equipment.byReservationOnly
+                      ? <Stack direction={"row"} spacing={1} alignItems="center" padding="10px">
+                        {
+                          hasApprovedAccessCheck
+                            ? <CheckCircleIcon color="success" />
+                            : <CloseIcon color="error" />
                         }
-
-                        <Stack height="100%" width={"100%"}>
-                            {/* Title & Edit button */}
-                            <Stack direction="row" justifyContent="space-between" pl={"10px"}>
-                                <Typography variant="h6">{props.equipment.archived ? `${props.equipment.name} (Hidden)` : props.equipment.name}</Typography>
-                                {
-                                    isPriviledged
-                                        ? <Button
-                                            onClick={() => { navigate(`/admin/equipment/${props.equipment.archived ? "archived/" : ""}${props.equipment.id}`) }}
-                                            aria-label="edit button"
-                                            sx={{ width: "40px", height: "40px" }}
-                                            variant="contained"
-                                            color="primary"
-                                        >
-                                            <ConstructionIcon />
-                                        </Button>
-                                        : null
-                                }
-                            </Stack>
-                            <Stack direction="row" justifyContent="space-between" height="100%">
-                                {/* Trainings & Access Check */}
-                                <Stack width="100%">
-                                    {moduleStatuses.map((ms: ModuleStatus) => (
-                                        <ModuleStatusRow ms={ms} />
-                                    ))}
-                                    {
-                                        !props.equipment.byReservationOnly
-                                            ? <Stack direction={"row"} spacing={1} alignItems="center" padding="10px">
-                                                {
-                                                    hasApprovedAccessCheck
-                                                        ? <CheckCircleIcon color="success" />
-                                                        : <CloseIcon color="error" />
-                                                }
-                                                <Typography variant="body2">In-Person Competency Check</Typography>
-                                            </Stack>
-                                            : null
-                                    }
-                                </Stack>
-                                {/* Num available || by reservation only */}
-                                <Stack width="120px" height="100%" justifyContent={"center"} alignItems={"center"}>
-                                    {props.equipment.byReservationOnly
-                                        ? <Typography variant="subtitle1" ml={1}>
-                                            Reservation only. Email <Link to={"mailto:make@rit.edu"} target={"_blank"}>make@rit.edu</Link> to schedule.
-                                        </Typography>
-                                        : props.equipment.numAvailable + props.equipment.numInUse > 0 ?
-                                            <Stack height="100%" justifyContent="center" alignItems="center">
-                                                <Typography variant="subtitle1" align="center" fontWeight="bold">
-                                                    Machines Available
-                                                </Typography>
-                                                <Typography variant="subtitle1" align="center">
-                                                    {`${props.equipment.numAvailable} / ${props.equipment.numAvailable + props.equipment.numInUse}`}
-                                                </Typography>
-                                            </Stack>
-                                            :
-                                            <></>
-                                    }
-                                </Stack>
-                            </Stack>
-
-                        </Stack>
-                    </Stack>
-                    {/* Desc && learn more */}
-                    <Stack justifyContent={"space-between"} height={"inherit"}>
-                        <Typography>
-                            <ReactMarkdown>{props.equipment.notes}</ReactMarkdown>
-                        </Typography>
-                        <Button
-                            size="small"
-                            variant="contained"
-                            color="info"
-                            onClick={() => window.open(props.equipment.sopUrl, "_blank")}
-                            sx={{ alignSelf: "flex-end" }}
-                        >
-                            Learn More
-                        </Button>
-                    </Stack>
+                        <Typography variant="body2">In-Person Competency Check</Typography>
+                      </Stack>
+                      : null
+                  }
                 </Stack>
-            </CardContent>
-        </Card>
-    );
+                {/* Num available || by reservation only */}
+                <Stack width="120px" height="100%" justifyContent={"center"} alignItems={"center"}>
+                  {props.equipment.byReservationOnly
+                    ? <Typography variant="subtitle1" ml={1}>
+                      Reservation only. Email <Link to={"mailto:make@rit.edu"} target={"_blank"}>make@rit.edu</Link> to schedule.
+                    </Typography>
+                    : props.equipment.numAvailable + props.equipment.numInUse > 0 ?
+                      <Stack height="100%" justifyContent="center" alignItems="center">
+                        <Typography variant="subtitle1" align="center" fontWeight="bold">
+                          Machines Available
+                        </Typography>
+                        <Typography variant="subtitle1" align="center">
+                          {`${props.equipment.numAvailable} / ${props.equipment.numAvailable + props.equipment.numInUse}`}
+                        </Typography>
+                      </Stack>
+                      :
+                      <></>
+                  }
+                </Stack>
+              </Stack>
+
+            </Stack>
+          </Stack>
+          {/* Desc && learn more */}
+          <Stack justifyContent={"space-between"} height={"inherit"}>
+            <Typography>
+              <ReactMarkdown>{props.equipment.notes}</ReactMarkdown>
+            </Typography>
+            <Button
+              size="small"
+              variant="contained"
+              color="info"
+              onClick={() => window.open(props.equipment.sopUrl, "_blank")}
+              sx={{ alignSelf: "flex-end" }}
+            >
+              Learn More
+            </Button>
+          </Stack>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 }
