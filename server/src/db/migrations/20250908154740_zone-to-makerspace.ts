b0e234d20d0231c07f9c3075f2fa9123fdc13bf2
cowsed <richie.sommers@outlook.com>
2025-09-08 12:44:06 -0400

    migrate more

--- server/src/db/migrations/20250908154740_zone-to-makerspace.ts
+++ server/src/db/migrations/20250908154740_zone-to-makerspace.ts
@@ -2,11 +2,18 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.alterTable("Rooms", (t)=>{
+    if (await knex.schema.hasTable("Makerspaces")) {
+        // already did it
+        return;
+    }
+    await knex.schema.renameTable("Zones", "Makerspaces");
+
+    await knex.schema.alterTable("Rooms", (t) => {
         t.renameColumn("zoneID", "makerspaceID");
     });
-    await knex.schema.renameTable("Zones", "Makerspaces");
-    await knex.schema.alterTable("Makerspaces", (t)=>{
+
+
+    await knex.schema.alterTable("Makerspaces", (t) => {
         t.string("subtitle");
         t.string("location");
     })
@@ -15,5 +22,18 @@ export async function up(knex: Knex): Promise<void> {
 
 
 export async function down(knex: Knex): Promise<void> {
+    if (await knex.schema.hasTable("Zones")) {
+        // already didnt do it
+        return;
+    }
+
+    await knex.schema.renameTable("Makerspaces", "Zones");
+
+    await knex.schema.alterTable("Rooms", (t) => {
+        t.renameColumn("makerspaceID", "zoneID");
+    });
+    await knex.schema.alterTable("Makerspaces", (t) => {
+        t.dropColumns("subtitle", "location");
+    })
 }
 
