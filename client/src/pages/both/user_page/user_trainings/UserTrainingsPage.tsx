bac405b214f95a53420d8e09041a268da3a81d64
Sierra Tran <smt9483@g.rit.edu>
2025-09-12 12:35:08 -0400

    spaces (#983)
    Co-authored-by: cowsed <richie.sommers@outlook.com>

--- client/src/pages/both/user_page/user_trainings/UserTrainingsPage.tsx
+++ client/src/pages/both/user_page/user_trainings/UserTrainingsPage.tsx
@@ -3,8 +3,8 @@ import { useCurrentUser } from "../../../../common/CurrentUserProvider";
 import { GET_ALL_TRAINING_MODULES } from "../../../maker/training/TrainingPage";
 import { useQuery } from "@apollo/client";
 import {
-	ModuleStatus,
-	moduleStatusMapper,
+  ModuleStatus,
+  moduleStatusMapper,
 } from "../../../../common/TrainingModuleUtils";
 import RequestWrapper2 from "../../../../common/RequestWrapper2";
 import { GET_ACCESS_CHECKS_BY_USERID } from "../../../../queries/accessChecksQueries";
@@ -20,131 +20,131 @@ import { useIsMobile } from "../../../../common/IsMobileProvider";
  * @returns The UserTrainingsPage component that displays the user's trainings and equipment.
  */
 export default function UserTraingingsPage() {
-	const user = useCurrentUser();
-	const isMobile = useIsMobile();
+  const user = useCurrentUser();
+  const isMobile = useIsMobile();
 
-	const getAllModules = useQuery(GET_ALL_TRAINING_MODULES);
-	const getAccessChecks = useQuery(GET_ACCESS_CHECKS_BY_USERID, {
-		variables: { userID: user.id },
-	});
+  const getAllModules = useQuery(GET_ALL_TRAINING_MODULES);
+  const getAccessChecks = useQuery(GET_ACCESS_CHECKS_BY_USERID, {
+    variables: { userID: user.id },
+  });
 
-	return (
-		<Stack
-			spacing={2}
-			margin={isMobile ? "10px" : "20px"}
-			width="fit-content"
-			divider={<Divider orientation="horizontal" flexItem />}
-		>
-			{/* Trainings */}
-			<RequestWrapper2
-				result={getAllModules}
-				render={({ modules }) => {
-					const moduleStatuses = modules
-						.map(moduleStatusMapper(user.passedModules, user.trainingHolds))
-						.filter((ms: ModuleStatus) => !ms.archived);
+  return (
+    <Stack
+      spacing={2}
+      margin={isMobile ? "10px" : "20px"}
+      width="fit-content"
+      divider={<Divider orientation="horizontal" flexItem />}
+    >
+      {/* Trainings */}
+      <RequestWrapper2
+        result={getAllModules}
+        render={({ modules }) => {
+          const moduleStatuses = modules
+            .map(moduleStatusMapper(user.passedModules, user.trainingHolds))
+            .filter((ms: ModuleStatus) => !ms.archived);
 
-					const passed = moduleStatuses.filter(
-						(ms: ModuleStatus) => ms.status === "Passed"
-					);
+          const passed = moduleStatuses.filter(
+            (ms: ModuleStatus) => ms.status === "Passed"
+          );
 
-					const expiring = moduleStatuses.filter(
-						(ms: ModuleStatus) => ms.status === "Expiring Soon"
-					);
+          const expiring = moduleStatuses.filter(
+            (ms: ModuleStatus) => ms.status === "Expiring Soon"
+          );
 
-					return (
-						<Stack
-							spacing={3}
-							direction={isMobile ? "column" : "row"}
-							justifyContent={isMobile ? "center" : "space-between"}
-							divider={
-								isMobile ? (
-									<Divider orientation="horizontal" flexItem />
-								) : (
-									<Divider orientation="vertical" flexItem />
-								)
-							}
-							height={isMobile ? undefined : "30vh"}
-							width="100%"
-						>
-							{/* Complete Trainings */}
-							<Stack width={isMobile ? "auto" : "50%"} overflow="auto">
-								<Typography variant="h4" alignSelf="center">
-									Passed Trainings
-								</Typography>
-								{passed.map((ms: ModuleStatus) => (
-									<ModuleStatusRow ms={ms} />
-								))}
-							</Stack>
-							{/* Expiring Soon */}
-							<Stack width={isMobile ? "auto" : "50%"} overflow="auto">
-								<Typography variant="h4" alignSelf="center">
-									Trainings Expiring Soon
-								</Typography>
-								{expiring.map((ms: ModuleStatus) => (
-									<ModuleStatusRow ms={ms} />
-								))}
-							</Stack>
-						</Stack>
-					);
-				}}
-			/>
-			{/* Equipment */}
-			<RequestWrapper2
-				result={getAccessChecks}
-				render={({ accessChecksByUserID }) => {
-					const unarchived = accessChecksByUserID.filter(
-						(ac: AccessCheck) => !ac.equipment.archived
-					);
+          return (
+            <Stack
+              spacing={3}
+              direction={isMobile ? "column" : "row"}
+              justifyContent={isMobile ? "center" : "space-between"}
+              divider={
+                isMobile ? (
+                  <Divider orientation="horizontal" flexItem />
+                ) : (
+                  <Divider orientation="vertical" flexItem />
+                )
+              }
+              height={isMobile ? undefined : "30vh"}
+              width="100%"
+            >
+              {/* Complete Trainings */}
+              <Stack width={isMobile ? "auto" : "50%"} overflow="auto">
+                <Typography variant="h4" alignSelf="center">
+                  Passed Trainings
+                </Typography>
+                {passed.map((ms: ModuleStatus) => (
+                  <ModuleStatusRow ms={ms} />
+                ))}
+              </Stack>
+              {/* Expiring Soon */}
+              <Stack width={isMobile ? "auto" : "50%"} overflow="auto">
+                <Typography variant="h4" alignSelf="center">
+                  Trainings Expiring Soon
+                </Typography>
+                {expiring.map((ms: ModuleStatus) => (
+                  <ModuleStatusRow ms={ms} />
+                ))}
+              </Stack>
+            </Stack>
+          );
+        }}
+      />
+      {/* Equipment */}
+      <RequestWrapper2
+        result={getAccessChecks}
+        render={({ accessChecksByUserID }) => {
+          const unarchived = accessChecksByUserID.filter(
+            (ac: AccessCheck) => !ac.equipment.archived
+          );
 
-					const approved = unarchived.filter((ac: AccessCheck) => ac.approved);
+          const approved = unarchived.filter((ac: AccessCheck) => ac.approved);
 
-					const unapproved = unarchived.filter(
-						(ac: AccessCheck) => !ac.approved && !ac.equipment.byReservationOnly
-					);
+          const unapproved = unarchived.filter(
+            (ac: AccessCheck) => !ac.approved && !ac.equipment.byReservationOnly
+          );
 
-					return (
-						<Stack spacing={1}>
-							<Typography variant="h4">Approved Equipment</Typography>
-							<Grid
-								container
-								justifyContent="space-around"
-								width="fit-content"
-								rowSpacing={2}
-							>
-								{approved.map((ac: AccessCheck) => (
-									<Grid key={ac.equipment.id}>
-										<EquipmentCard
-											equipment={ac.equipment}
-											isMobile={isMobile}
-											staffMode={false}
-										/>
-									</Grid>
-								))}
-							</Grid>
-							<Typography variant="h4">
-								Awaiting In-person Knowledge Check
-							</Typography>
-							<Grid
-								container
-								justifyContent="space-around"
-								width="fit-content"
-								rowSpacing={2}
-							>
-								{unapproved.map((ac: AccessCheck) => (
-									<Grid key={ac.equipment.id}>
-										<EquipmentCard
-											equipment={ac.equipment}
-											isMobile={isMobile}
-											staffMode={false}
-										/>
-									</Grid>
-								))}
-							</Grid>
-						</Stack>
-					);
-				}}
-			/>
-			<title>{`${user.firstName}'s Trainings | Make @ RIT`}</title>
-		</Stack>
-	);
+          return (
+            <Stack spacing={1}>
+              <Typography variant="h4">Approved Equipment</Typography>
+              <Grid
+                container
+                justifyContent="space-around"
+                width="fit-content"
+                rowSpacing={2}
+              >
+                {approved.map((ac: AccessCheck) => (
+                  <Grid key={ac.equipment.id}>
+                    <EquipmentCard
+                      equipment={ac.equipment}
+                      isMobile={isMobile}
+                      staffMode={false}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+              <Typography variant="h4">
+                Awaiting In-person Knowledge Check
+              </Typography>
+              <Grid
+                container
+                justifyContent="space-around"
+                width="fit-content"
+                rowSpacing={2}
+              >
+                {unapproved.map((ac: AccessCheck) => (
+                  <Grid key={ac.equipment.id}>
+                    <EquipmentCard
+                      equipment={ac.equipment}
+                      isMobile={isMobile}
+                      staffMode={false}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            </Stack>
+          );
+        }}
+      />
+      <title>{`${user.firstName}'s Trainings | Make @ RIT`}</title>
+    </Stack>
+  );
 }
