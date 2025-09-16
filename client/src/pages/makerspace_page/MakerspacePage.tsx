b0744205c1842f7fa3ec1d97c168e5f04c40c4d9
SierraTran <sierra.tran@mail.com>
2025-09-16 16:51:59 -0400

    Thinking of layout ideas (w.i.p)

--- client/src/pages/makerspace_page/MakerspacePage.tsx
+++ client/src/pages/makerspace_page/MakerspacePage.tsx
@@ -9,13 +9,15 @@ import Room from "../../types/Room";
 import SearchBar from "../../common/SearchBar";
 import StaffBar from "./StaffBar";
 import { useCurrentUser } from "../../common/CurrentUserProvider";
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
 import { useIsMobile } from "../../common/IsMobileProvider";
 import { isManagerFor, isStaffFor } from "../../common/PrivilegeUtils";
 import { ModuleStatus, moduleStatusMapper } from "../../common/TrainingModuleUtils";
 import MakerspaceHoursSection from "./MakerspaceHours";
 import ModuleStatusRow from "../../common/ModuleStatusRow";
+import { is } from "immer/dist/internal.js";
+import { Link } from "react-router-dom";
 
 export default function MakerspacePage() {
   const { makerspaceID } = useParams<{ makerspaceID: string }>();
@@ -24,86 +26,139 @@ export default function MakerspacePage() {
   const navigate = useNavigate();
   const isMobile = useIsMobile();
 
-  const getMakerspace = useQuery(GET_MAKERSPACE_BY_ID, { variables: { id: makerspaceID } });
+  const getMakerspace = useQuery(GET_MAKERSPACE_BY_ID, {
+    variables: { id: makerspaceID },
+  });
 
   const [equipmentSearch, setEquipmentSearch] = useState("");
 
-  const staffMode = isStaffFor(user, Number(makerspaceID))
+  const staffMode = isStaffFor(user, Number(makerspaceID));
   const [showHidden, setShowHidden] = useState(false);
 
   return (
-    <RequestWrapper2 result={getMakerspace} render={(data) => {
+    <RequestWrapper2
+      result={getMakerspace}
+      render={(data) => {
+        const fullSpace: FullMakerspace = data.makerspaceByID;
 
-      const fullSpace: FullMakerspace = data.makerspaceByID;
+        const makerspaceTrainings = fullSpace.trainingModules.map(
+          moduleStatusMapper(user.passedModules, user.trainingHolds)
+        );
 
-      const makerspaceTrainings = fullSpace.trainingModules.map(moduleStatusMapper(user.passedModules, user.trainingHolds));
-
-      return (
-        <Stack spacing={"2"} padding={"0 20px 20px"} divider={<Divider orientation="horizontal" flexItem />}>
-          <StaffBar />
-          <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} width="auto">
-            <title>{`${fullSpace.name} | Make @ RIT`}</title>
-            <Typography variant="h3" align="center">{fullSpace.name}</Typography>
-            {
-              isManagerFor(user, Number(makerspaceID))
-                ? <IconButton
-                  onClick={() => { navigate(`/makerspace/${makerspaceID}/edit`) }}
+        return (
+          <Stack spacing={"2"} padding={"0 20px 20px"} divider={<Divider orientation="horizontal" flexItem />}>
+            <StaffBar />
+            <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} width="auto">
+              <title>{`${fullSpace.name} | Make @ RIT`}</title>
+              <Typography variant="h3" align="center">
+                {fullSpace.name}
+              </Typography>
+              {isManagerFor(user, Number(makerspaceID)) ? (
+                <IconButton
+                  onClick={() => {
+                    navigate(`/makerspace/${makerspaceID}/edit`);
+                  }}
                   sx={{ color: "gray" }}
                 >
                   <EditIcon />
                 </IconButton>
-                : null
-            }
-
-          </Stack>
-          <MakerspaceHoursSection hours={fullSpace.hours} isMobile={isMobile} />
-          {
-            makerspaceTrainings.length > 0 &&
-            <Stack direction={"column"} alignItems={"center"} padding={"10px 0"} spacing={1}>
-              <Stack direction={isMobile ? "column" : "row"} spacing={2} alignItems={"center"}>
-                <Typography variant="h6">Makerspace Trainings</Typography>
-                {
-                  makerspaceTrainings.some((ms) => (ms.status !== "Passed" && ms.status !== "Expiring Soon"))
-                    ? <Alert severity="error">You must pass the makerspace trainings before you can use equipment in the makerspace!</Alert>
-                    : null
-                }
-              </Stack>
-              <Stack direction={isMobile ? "column" : "row"} spacing={1} alignItems={"center"}>
-                {
-                  makerspaceTrainings.map((ms: ModuleStatus) => (
-                    <ModuleStatusRow ms={ms} />
-                  ))
-                }
-              </Stack>
+              ) : null}
             </Stack>
-          }
-          <Stack padding={"10px"} direction="row" spacing={2}>
-            <SearchBar
-              placeholder="Search Equipment"
-              value={equipmentSearch}
-              onChange={(e) => setEquipmentSearch(e.target.value)}
-              onClear={() => setEquipmentSearch("")}
-            />
-            {
-              isManagerFor(user, Number(makerspaceID)) && (
+
+            {/* Hours Section */}
+            <Stack>
+              <Typography variant="h4" align="center">
+                Hours
+              </Typography>
+              <MakerspaceHoursSection hours={fullSpace.hours} isMobile={isMobile} />
+            </Stack>
+
+            {/* Location & Description; Trainings & Equipment Doc Link; Makerspace Trainings */}
+            <Stack
+              direction={isMobile ? "column" : "row"}
+              divider={<Divider orientation={isMobile ? "horizontal" : "vertical"} flexItem />}
+              spacing={4}
+            >
+              {/* Location & Description Section */}
+              <Stack>
+                <Typography typography="h6">Location: {fullSpace.location}</Typography>
+                <Typography variant="body1">
+                  This is a description. It's longer then the subtitle but not too long. Just 1-2 sentences.
+                </Typography>
+              </Stack>
+
+              {/* Trainings & Equipment Doc Link Section */}
+              <Stack>
+                <Typography variant="h3" align="center">
+                  Trainings & Equipment
+                </Typography>
+                <Typography variant="body1" align="center">
+                  Learn more about our trainings and equipment{" "}
+                  <Link target="_blank" rel="noopener noreferrer" to={""}>
+                    here
+                  </Link>
+                  .
+                </Typography>
+              </Stack>
+
+              {/* Trainings Section */}
+              {makerspaceTrainings.length > 0 && (
+                <Stack direction={"column"} alignItems={"center"} padding={"10px 0"} spacing={1}>
+                  <Stack direction={isMobile ? "column" : "row"} spacing={2} alignItems={"center"}>
+                    <Typography variant="h6">Makerspace Trainings</Typography>
+                    {makerspaceTrainings.some((ms) => ms.status !== "Passed" && ms.status !== "Expiring Soon") ? (
+                      <Alert severity="error">
+                        You must pass the makerspace trainings before you can use equipment in the makerspace!
+                      </Alert>
+                    ) : null}
+                  </Stack>
+                  <Stack direction={isMobile ? "column" : "row"} spacing={1} alignItems={"center"}>
+                    {makerspaceTrainings.map((ms: ModuleStatus) => (
+                      <ModuleStatusRow ms={ms} />
+                    ))}
+                  </Stack>
+                </Stack>
+              )}
+            </Stack>
+
+            <Stack padding={"10px"} direction="row" spacing={2}>
+              <SearchBar
+                placeholder="Search Equipment"
+                value={equipmentSearch}
+                onChange={(e) => setEquipmentSearch(e.target.value)}
+                onClear={() => setEquipmentSearch("")}
+              />
+              {isManagerFor(user, Number(makerspaceID)) && (
                 <Stack direction={isMobile ? "column" : "row"} spacing={2}>
-                  <Button variant="contained" color="success" startIcon={<AddIcon />} onClick={() => (navigate(`/makerspace/${makerspaceID}/equipment/new`))}>
+                  <Button
+                    variant="contained"
+                    color="success"
+                    startIcon={<AddIcon />}
+                    onClick={() => navigate(`/makerspace/${makerspaceID}/equipment/new`)}
+                  >
                     Create New Equipment
                   </Button>
                   <FormControlLabel
                     control={<Switch checked={showHidden} onChange={(e) => setShowHidden(e.target.checked)} />}
                     label={"Show Hidden Equipment"}
-                    labelPlacement="start" />
+                    labelPlacement="start"
+                  />
                 </Stack>
-              )
-            }
-          </Stack>
+              )}
+            </Stack>
 
-          {fullSpace.rooms.map((room: Room) => (
-            <RoomSection room={room} equipmentSearch={equipmentSearch} isMobile={isMobile} staffMode={staffMode} showHidden={showHidden} />
-          ))}
-        </Stack>
-      );
-    }} />
+            {fullSpace.rooms.map((room: Room) => (
+              <RoomSection
+                room={room}
+                equipmentSearch={equipmentSearch}
+                isMobile={isMobile}
+                staffMode={staffMode}
+                showHidden={showHidden}
+              />
+            ))}
+          </Stack>
+        );
+      }}
+    />
   );
 }
