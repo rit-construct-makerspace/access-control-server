9a58d1169022c125ae4b66e642bf1403def82e19
SierraTran <sierra.tran@mail.com>
2025-09-26 11:42:21 -0400

    Add effect for setting mobile menu based on isMobile

--- client/src/pages/makerspace_page/StaffBar.tsx
+++ client/src/pages/makerspace_page/StaffBar.tsx
@@ -1,23 +1,23 @@
-import { ButtonBase, Stack, Typography } from "@mui/material";
-import { useCurrentUser } from "../../common/CurrentUserProvider";
-import NavLink from "../../top_nav/NavLink";
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import ArchitectureIcon from '@mui/icons-material/Architecture';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import HistoryIcon from "@mui/icons-material/History";
 import InventoryIcon from "@mui/icons-material/Inventory";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import MenuIcon from '@mui/icons-material/Menu';
+import PaidIcon from '@mui/icons-material/Paid';
+import PeopleIcon from "@mui/icons-material/People";
+import ScannerIcon from '@mui/icons-material/Scanner';
 import SchoolIcon from "@mui/icons-material/School";
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
-import PeopleIcon from "@mui/icons-material/People";
-import HistoryIcon from "@mui/icons-material/History";
-import BarChartIcon from '@mui/icons-material/BarChart';
-import ArchitectureIcon from '@mui/icons-material/Architecture';
-import { useState } from "react";
-import MenuIcon from '@mui/icons-material/Menu';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import ScannerIcon from '@mui/icons-material/Scanner';
-import { isOnlyTrainer, isStaffFor } from "../../common/PrivilegeUtils";
-import { useIsMobile } from "../../common/IsMobileProvider";
+import { ButtonBase, Stack, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
 import { Outlet, useParams } from "react-router-dom";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import PaidIcon from '@mui/icons-material/Paid';
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import { useCurrentUser } from "../../common/CurrentUserProvider";
+import { useIsMobile } from "../../common/IsMobileProvider";
+import { isOnlyTrainer, isStaffFor } from "../../common/PrivilegeUtils";
+import NavLink from "../../top_nav/NavLink";
 
 export default function StaffBar() {
   const { makerspaceID } = useParams<{ makerspaceID: string }>();
@@ -28,6 +28,12 @@ export default function StaffBar() {
   const isTrainer = isOnlyTrainer(user);
 
   const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    if (!isMobile && mobileMenu) {
+      setMobileMenu(false);
+    }
+  }, [isMobile, mobileMenu]);
 
   if (!isPriviledged && !isTrainer) {
     return null;
