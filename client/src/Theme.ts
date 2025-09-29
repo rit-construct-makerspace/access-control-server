af28fed82376d0d693f338f0378e4bc019c10531
cowsed <richie.sommers@outlook.com>
2025-09-29 12:06:42 -0400

    update icon and secondary color

--- client/src/Theme.ts
+++ client/src/Theme.ts
@@ -88,7 +88,7 @@ class ItalianRestaurant implements MakeTheme {
         contrastText: "#FFFFFF",
       },
       secondary: {
-        main: "#7D55C7",
+        main: "#54301a",
         contrastText: "#FFFFFF",
       },
       warning: {
