c3f56795cb8a32dd3d9f5b46f413ae1dbfe7758e
SierraTran <sierra.tran@mail.com>
2025-10-01 18:10:07 -0400

    Restore description and docsLink for makerspacesSchema

--- server/src/schemas/makerspacesSchema.ts
+++ server/src/schemas/makerspacesSchema.ts
@@ -6,43 +6,52 @@
 import { gql } from "graphql-tag";
 
 export interface MakerspaceInput {
-  name: string;
-  subtitle: string | null;
-  location: string | null;
-  imageUrl: string;
+	name: string;
+	subtitle: string | null;
+	location: string | null;
+	description: string;
+	docsLink: string;
+	imageUrl: string;
 }
 
 export const MakerspacesTypeDefs = gql`
-  type Makerspace {
-    id: ID!
-    name: String!
-    subtitle: String
-    location: String
-    rooms: [Room]
-    hours: [MakerspaceHours]
-    items: [InventoryItem]
-    imageUrl: String
-    trainingModules: [TrainingModule]
-  }
+	type Makerspace {
+		id: ID!
+		name: String!
+		subtitle: String
+		location: String
+		description: String
+		docsLink: String
+		rooms: [Room]
+		hours: [MakerspaceHours]
+		items: [InventoryItem]
+		imageUrl: String
+		trainingModules: [TrainingModule]
+	}
 
-  input MakerspaceInput {
-    name: String!
-    subtitle: String
-    location: String
-    imageUrl: String
-  }
+	input MakerspaceInput {
+		name: String!
+		subtitle: String
+		location: String
+		description: String
+		docsLink: String
+		imageUrl: String
+	}
 
-  extend type Query {
-    makerspaces(storefrontVisible: Boolean): [Makerspace]
-    makerspaceByID(id: ID!): Makerspace
-  }
+	extend type Query {
+		makerspaces(storefrontVisible: Boolean): [Makerspace]
+		makerspaceByID(id: ID!): Makerspace
+	}
 
-  extend type Mutation {
-    deleteMakerspace(id: ID!): Makerspace
-    addMakerspace(name: String!): Makerspace
-    archiveMakerspace(id: ID!): Makerspace
-    updateMakerspace(id: ID!, newMakerspace: MakerspaceInput): Makerspace
-    addTrainingToMakerspace(makerspaceID: ID!, moduleID: ID!): [TrainingModule]
-    removeTrainingFromMakerspace(makerspaceID: ID!, moduleID: ID!): [TrainingModule]
-  }
+	extend type Mutation {
+		deleteMakerspace(id: ID!): Makerspace
+		addMakerspace(name: String!): Makerspace
+		archiveMakerspace(id: ID!): Makerspace
+		updateMakerspace(id: ID!, newMakerspace: MakerspaceInput): Makerspace
+		addTrainingToMakerspace(makerspaceID: ID!, moduleID: ID!): [TrainingModule]
+		removeTrainingFromMakerspace(
+			makerspaceID: ID!
+			moduleID: ID!
+		): [TrainingModule]
+	}
 `;
