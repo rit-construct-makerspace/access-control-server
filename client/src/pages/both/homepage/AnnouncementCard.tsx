7f4f1354f3716d0df07ce4913b69746d5ccb7e06
SierraTran <sierra.tran@mail.com>
2025-10-16 13:48:02 -0400

    Add link button in card actions if the announcement has one

--- client/src/pages/both/homepage/AnnouncementCard.tsx
+++ client/src/pages/both/homepage/AnnouncementCard.tsx
@@ -1,19 +1,37 @@
-import { Card, CardContent, Typography, useTheme } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Typography, useTheme } from "@mui/material";
 import { Announcement } from "../../../queries/announcementsQueries";
 import ThemedMarkdown from "../../../common/ThemedMarkdown";
 
 interface AnnouncementCardProps {
-    announcement: Announcement;
+  announcement: Announcement;
 }
 
 export default function AnnouncementCard(props: AnnouncementCardProps) {
-    const theme = useTheme();
-    return (
-        <Card sx={{ height: "100%" }}>
-            <CardContent>
-                <Typography color={theme.palette.primary.main} variant="h5">{props.announcement.title}</Typography>
-                <Typography variant="body1"><ThemedMarkdown>{props.announcement.description}</ThemedMarkdown></Typography>
-            </CardContent>
-        </Card>
-    );
+  const theme = useTheme();
+  return (
+    <Card sx={{ height: "100%" }}>
+      <CardContent>
+        <Typography color={theme.palette.primary.main} variant="h5">
+          {props.announcement.title}
+        </Typography>
+        <Typography variant="body1">
+          <ThemedMarkdown>{props.announcement.description}</ThemedMarkdown>
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ justifyContent: 'flex-end' }}>
+        {props.announcement.linkText || props.announcement.linkUrl ? (
+          <Button
+            variant="contained"
+            color="info"
+            size="small"
+            href={props.announcement.linkUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {props.announcement.linkText}
+          </Button>
+        ) : null}
+      </CardActions>
+    </Card>
+  );
 }
