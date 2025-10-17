d063bc2cf5bffa65e1e1c26beeedf31f24b5c755
Yi Xhan <yl9431@rit.edu>
2025-10-17 12:24:17 -0400

    missed comma and place where this can be used

--- client/src/common/ImageFinder.tsx
+++ client/src/common/ImageFinder.tsx
@@ -1,4 +1,4 @@
-export function findImage(image: string|undefined|null, path= "", ){
+export function findImage(image: string|undefined|null, path= ""){
     const result = import.meta.env.VITE_CDN_URL + path + image
     return (image && image !== "") ? result : import.meta.env.BASE_URL + "/shed_acronym_vert.jpg"
 }
