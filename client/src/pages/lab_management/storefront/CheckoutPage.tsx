3d23cb0f661d8fe7727344ed03349eb4c17032c9
cowsed <richie.sommers@outlook.com>
2025-10-17 12:52:46 -0400

    starting checkout

---
+++ client/src/pages/lab_management/storefront/CheckoutPage.tsx
@@ -0,0 +1,10 @@
+import { Autocomplete } from "@mui/material";
+import { Stack } from "@mui/system";
+
+export default function CheckoutPage(){
+    return <Stack direction="column">
+        <title>Checkout | Make @ RIT</title>
+        asdfghjkl
+        <Autocomplete />
+    </Stack>
+}
