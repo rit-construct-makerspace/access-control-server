b4928b3ec776c201f86bdc101015b8c67259e0c8
SierraTran <sierra.tran@mail.com>
2025-10-24 12:18:16 -0400

    Apply neutral icons on module status rows for users that aren't logged in

--- client/src/common/ModuleStatusRow.tsx
+++ client/src/common/ModuleStatusRow.tsx
@@ -2,47 +2,53 @@ import { CardActionArea, Link, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { ModuleStatus } from "./TrainingModuleUtils";
 import WarningIcon from "@mui/icons-material/Warning";
-import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
-import LockClockIcon from '@mui/icons-material/LockClock';
+import HourglassBottomIcon from "@mui/icons-material/HourglassBottom";
+import LockClockIcon from "@mui/icons-material/LockClock";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CloseIcon from "@mui/icons-material/Close";
 import { format } from "date-fns";
 import { useQuery } from "@apollo/client";
 import { GET_ACCESS_PROGRESSES } from "../queries/trainingQueries";
 import { GET_PASSED_SUBMISSION } from "../queries/getSubmissions";
+import { useCurrentUser } from "./CurrentUserProvider.js";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
 interface ModuleStatusRowProps {
-  ms: ModuleStatus
+  ms: ModuleStatus;
 }
 
 export default function ModuleStatusRow(props: ModuleStatusRowProps) {
   const navigate = useNavigate();
-  const passedSubmission = useQuery(GET_PASSED_SUBMISSION, {variables: {moduleID: props.ms.moduleID}});
-  const navigateUrl = (!passedSubmission?.data?.passingSubmission?.id) ? `/maker/training/${props.ms.moduleID}` : `/maker/training/${props.ms.moduleID}/results/${passedSubmission?.data?.passingSubmission?.id}`
+  const user = useCurrentUser();
+  const passedSubmission = useQuery(GET_PASSED_SUBMISSION, { variables: { moduleID: props.ms.moduleID } });
+  const navigateUrl = !passedSubmission?.data?.passingSubmission?.id
+    ? `/maker/training/${props.ms.moduleID}`
+    : `/maker/training/${props.ms.moduleID}/results/${passedSubmission?.data?.passingSubmission?.id}`;
 
   return (
     <CardActionArea onClick={() => navigate(navigateUrl)} sx={{ width: "unset" }}>
       <Stack direction="row" spacing={1} alignItems="center" padding="10px" width="100%">
-        {
-          props.ms.status === "Passed"
-            ? <CheckCircleIcon color="success" />
-            : props.ms.status === "Not taken"
-              ? <CloseIcon color="error" />
-              : props.ms.status === "Expired"
-                ? <WarningIcon color="warning" />
-                : props.ms.status === "Expiring Soon"
-                  ? <HourglassBottomIcon color="warning" />
-                  : props.ms.status === "Locked"
-                    ? <LockClockIcon color="error" />
-                    : null
-        }
+        {user.visitor ? (
+          <RadioButtonUncheckedIcon color="secondary" />
+        ) : props.ms.status === "Passed" ? (
+          <CheckCircleIcon color="success" />
+        ) : props.ms.status === "Not taken" ? (
+          <CloseIcon color="error" />
+        ) : props.ms.status === "Expired" ? (
+          <WarningIcon color="warning" />
+        ) : props.ms.status === "Expiring Soon" ? (
+          <HourglassBottomIcon color="warning" />
+        ) : props.ms.status === "Locked" ? (
+          <LockClockIcon color="error" />
+        ) : null}
         <Stack direction="column" width="100%">
-          <Link variant="body2" color="primary" width={"stretch"}>{props.ms.moduleName}</Link>
-          {
-            props.ms.status === "Passed" || props.ms.status === "Expiring Soon"
-              ? <Typography variant="body2">Expires: {format(new Date(props.ms.expirationDate), "MMM d, yyyy")}</Typography>
-              : null
-          }
+          <Link variant="body2" color="primary" width={"stretch"}>
+            {props.ms.moduleName}
+          </Link>
+          {props.ms.status === "Passed" || props.ms.status === "Expiring Soon" ? (
+            <Typography variant="body2">Expires: {format(new Date(props.ms.expirationDate), "MMM d, yyyy")}</Typography>
+          ) : null}
         </Stack>
       </Stack>
     </CardActionArea>
