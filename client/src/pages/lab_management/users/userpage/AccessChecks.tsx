5a740288292188fc23927b5e0c22465e1c32560e
Yi Xhan <yl9431@rit.edu>
2025-11-07 14:32:01 -0500

    user page components

---
+++ client/src/pages/lab_management/users/userpage/AccessChecks.tsx
@@ -0,0 +1,87 @@
+import { Alert, Button, Chip, MenuItem, Select, Stack, Typography } from "@mui/material"
+import ActionButton from "../../../../common/ActionButton"
+import { isManager, isStaffFor, isTrainerFor } from "../../../../common/PrivilegeUtils"
+import { useCurrentUser } from "../../../../common/CurrentUserProvider";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { AccessCheckExtraInfo, GET_USER } from "../../../../queries/userQueries";
+import { useState } from "react";
+import RequestWrapper from "../../../../common/RequestWrapper";
+import { GET_ALL_EQUIPMENTS } from "../../../../queries/equipmentQueries";
+import AccessCheckCard from "../AccessCheckCard";
+
+const REFRESH_CHECKS = gql`
+  mutation RefreshAccessChecks($userID: ID!) {
+    refreshAccessChecks(userID: $userID)
+  }
+`;
+
+const CREATE_CHECK = gql`
+  mutation CreateAccessCheck($userID: ID!, $equipmentID: ID!) {
+    createAccessCheck(userID: $userID, equipmentID: $equipmentID)
+  }
+`;
+
+interface AccessCheckProps {
+  user: any;
+}
+
+export default function AccessChecks(props: AccessCheckProps) {
+  const currentUser = useCurrentUser();
+
+  const [openCreateCheckDialouge, setOpenCreateCheckDialouge] = useState<boolean>();  
+  const [newCheckEquipmentID, setNewCheckEquipmentID] = useState<string>();
+  
+
+  const getEquipment = useQuery(GET_ALL_EQUIPMENTS);  
+  const [refreshCheck, refreshCheckResult] = useMutation(REFRESH_CHECKS, { variables: { userID: props.user.id }, refetchQueries: [{ query: GET_USER, variables: { id: props.user.id } }] });
+  const [createCheck] = useMutation(CREATE_CHECK, { refetchQueries: [{ query: GET_USER, variables: { id: props.user.id } }] });
+  
+  function handleCheckCreate() {
+    if (!newCheckEquipmentID) return;
+    createCheck({ variables: { userID: props.user.id, equipmentID: newCheckEquipmentID } });
+    setOpenCreateCheckDialouge(false);
+  }
+
+  const filteredACs: AccessCheckExtraInfo[] = props.user.accessChecks.filter(
+    (ac: AccessCheckExtraInfo) => (
+      ac.equipment.requiresTrainerApproval
+        ? isTrainerFor(currentUser, Number(ac.equipment.id), Number(ac.equipment.room.makerspace.id))
+        : (isStaffFor(currentUser, Number(ac.equipment.room.makerspace.id)) || isTrainerFor(currentUser, Number(ac.equipment.id), Number(ac.equipment.room.makerspace.id)))
+    )
+  );
+
+  return (
+    // TODO: check if this is the right width
+    <Stack width="50%">
+      <Typography variant="h6" component="div" mb={1}>
+        Access Checks
+      </Typography>
+
+      <Stack direction={"row"} spacing={1}>
+        <ActionButton iconSize={5} color="info" appearance={"small"} variant="outlined" handleClick={async () => { refreshCheck() }} loading={refreshCheckResult.loading} buttonText="Refresh Checks" tooltipText="Purge all unapproved checks and repopulate based on currently passed modules." />
+        {isManager(currentUser) && <ActionButton iconSize={5} color="primary" appearance={"small"} variant="outlined" handleClick={async () => { setOpenCreateCheckDialouge(!openCreateCheckDialouge) }} loading={false} buttonText="Create Check" />}
+      </Stack>
+      {openCreateCheckDialouge && <Stack direction={"row"} mt={1}>
+        <RequestWrapper loading={getEquipment.loading} error={getEquipment.error}>
+          <Select value={newCheckEquipmentID} onChange={(e) => setNewCheckEquipmentID(e.target.value)} sx={{ width: "50%" }}>
+            {getEquipment.data?.allEquipment.map((equipment: { id: number, name: string, archived: boolean }) => (
+              <MenuItem value={equipment.id}>{equipment.name} {equipment.archived && <Chip variant="outlined" color="warning" size="small" label="hidden" sx={{ ml: "1em" }} />}</MenuItem>
+            ))}
+          </Select>
+        </RequestWrapper>
+        <Button variant="outlined" color="success" onClick={handleCheckCreate}>Create</Button>
+      </Stack>}
+
+      <Stack spacing={1} mt={2}>
+        {filteredACs != null && filteredACs.map((accessCheck: AccessCheckExtraInfo) => (
+          <AccessCheckCard key={accessCheck.id} accessCheck={accessCheck} userID={props.user.id} />
+        ))}
+      </Stack>
+
+      {(filteredACs == null || (filteredACs.length === 0)) && (
+        <Alert severity="info">No Access Checks Available</Alert>
+      )}
+
+    </Stack>
+  )
+}
---
+++ client/src/pages/lab_management/users/userpage/HoldsRestrictions.tsx
@@ -0,0 +1,161 @@
+import { Alert, Button, FormControl, InputLabel, MenuItem, Select, Stack, Typography } from "@mui/material";
+import { useCurrentUser } from "../../../../common/CurrentUserProvider";
+import RequestWrapper2 from "../../../../common/RequestWrapper2";
+import LockIcon from '@mui/icons-material/Lock';
+import BlockIcon from '@mui/icons-material/Block';
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { GET_USER, Hold, Restriction } from "../../../../queries/userQueries";
+import HoldCard from "../HoldCard";
+import RestrictionCard from "../RestrictionCard";
+import { isStaff, isStaffFor } from "../../../../common/PrivilegeUtils";
+import { FullMakerspace, GET_FULL_MAKERSPACES } from "../../../../queries/makerspaceQueries";
+import { useState } from "react";
+
+const CREATE_HOLD = gql`
+  mutation CreateHold($userID: ID!, $description: String!) {
+    createHold(userID: $userID, description: $description) {
+      id
+    }
+  }
+`;
+
+const CREATE_RESTRICTION = gql`
+  mutation CreateRestriction($userID: ID!, $makerspaceID: ID!, $reason: String!) {
+    createRestriction(targetID: $userID, makerspaceID: $makerspaceID, reason: $reason) {
+      id
+    }
+  }
+`;
+
+interface HoldsRestrictionsProps {
+  user: any;
+}
+
+// ALSO RESTRICTIONS
+export default function HoldsRestrictions(props: HoldsRestrictionsProps) {
+  const currentUser = useCurrentUser();
+
+  const [restrictionMakerspace, setRestrictionMakerspace] = useState(-1);
+
+  const [createHold] = useMutation(CREATE_HOLD, { refetchQueries: [{ query: GET_USER, variables: { id: props.user.id } }] });
+  const [createRestriction] = useMutation(CREATE_RESTRICTION, { refetchQueries: [{ query: GET_USER, variables: { id: props.user.id } }] });
+  const getMakerspacesResult = useQuery(GET_FULL_MAKERSPACES);
+
+  const handlePlaceHoldClicked = () => {
+    const description = window.prompt("Enter hold description:");
+    if (description === "") {
+      window.alert("Description required.");
+      return;
+    }
+    else if (!description) {
+      return;
+    }
+
+    createHold({
+      variables: { userID: props.user.id, description }
+    });
+  };
+
+  function handleCreateRestriction() {
+    if (restrictionMakerspace === -1) {
+      window.alert("Makerspace Required");
+      return;
+    }
+
+    const reason = window.prompt("Enter reason for restriction:")
+    if (reason === "") {
+      window.alert("Reason required.");
+      return;
+    } else if (!reason) {
+      return;
+    }
+
+    createRestriction({
+      variables: { userID: props.user.id, makerspaceID: restrictionMakerspace, reason: reason }
+    });
+  }
+
+
+  return (
+    <Stack>
+      <Stack direction="row" justifyContent={"space-between"} mb={1}>
+        <Typography variant="h6">
+          Account Holds
+        </Typography>
+        <Button
+          color="error"
+          variant="contained"
+          onClick={handlePlaceHoldClicked}
+          startIcon={<BlockIcon />}
+        >
+          Place hold
+        </Button>
+      </Stack>
+
+      {props.user.holds.length === 0 && (
+        <Alert severity="success">No Holds!</Alert>
+      )}
+
+      <Stack spacing={2}>
+        {props.user.holds.map((hold: Hold) => (
+          <HoldCard key={hold.id} hold={hold} userID={props.user.id} />
+        ))}
+      </Stack>
+
+
+      <Typography variant="h6" component="div" mt={2} mb={1}>
+        Account Restrictions
+      </Typography>
+
+      {props.user.restrictions.length === 0 && (
+        <Alert severity="success">No Restrictions!</Alert>
+      )}
+
+      <Stack spacing={2}>
+        {props.user.restrictions.map((restriction: Restriction) => (
+          <RestrictionCard key={restriction.id} restriction={restriction} userID={props.user.id} />
+        ))}
+      </Stack>
+
+      {
+        isStaff(currentUser)
+          ? < RequestWrapper2 result={getMakerspacesResult} render={(data) => {
+
+            const fullSpaces: FullMakerspace[] = data.makerspaces;
+            // I hate typescript I hate typescript I hate typescript I hate typescript I hate typescript I hate typescript I hate typescript I hate typescript 
+            const potentialRestrictions = fullSpaces.filter((space: FullMakerspace) => isStaffFor(currentUser, Number(space.id)))
+
+            return (
+              <Stack direction="row" spacing={1} mt={2} alignItems={"center"}>
+                <FormControl fullWidth>
+                  <InputLabel id="restriction-makerspace">Makerspace</InputLabel>
+                  <Select id="restriction-makerspace"
+                    label="Makerspace"
+                    onChange={(e) => setRestrictionMakerspace(Number(e.target.value))}
+                    fullWidth
+                  >
+                    {
+                      potentialRestrictions.map((space: FullMakerspace) => (
+                        <MenuItem value={space.id}>{space.name} ID: {space.id}</MenuItem>
+                      ))
+                    }
+                  </Select>
+                </FormControl>
+                <Button
+                  variant="contained"
+                  color="error"
+                  onClick={handleCreateRestriction}
+                  startIcon={<LockIcon />}
+                  sx={{ whiteSpace: "nowrap", minWidth: "unset" }}
+                >
+                  Place Restriction
+                </Button>
+              </Stack>
+            );
+          }
+          } />
+          : null
+      }
+    </Stack>
+  )
+}
