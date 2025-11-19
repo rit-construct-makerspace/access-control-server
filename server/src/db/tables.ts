d6b54241ae163861871f40597fbe3c303483a543
Sierra Tran <smt9483@g.rit.edu>
2025-11-19 11:40:28 -0500

    Rename to Staff Sign-Off (#1179)

--- server/src/db/tables.ts
+++ server/src/db/tables.ts
@@ -168,7 +168,7 @@ export interface InventoryLedgerRow {
 
 /**
  * A record of a quiz submission.
- * Note, this table does not control access to equipment. Rather, the AccessChecks and PassedModules tables record successful In-person checks and Non-expired trainings respectively
+ * Note, this table does not control access to equipment. Rather, the AccessChecks and PassedModules tables record successful In-person checks (staff sign-offs) and Non-expired trainings respectively
  * PassedModules records trainings that have not expired (It is kept up to date by purging expired trainings)
  * ModuleSubmissions records all quiz attempts forever
  * AccessChecks records in person competency checks
