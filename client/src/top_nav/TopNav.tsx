9726ba60a2961c5ebce61bd6a92e056fc4651b5d
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-01-09 15:17:08 -0500

    Update client/src/top_nav/TopNav.tsx

--- client/src/top_nav/TopNav.tsx
+++ client/src/top_nav/TopNav.tsx
@@ -67,8 +67,8 @@ export default function TopNav() {
           </Alert>
           : null
       }
+    { // Lab training Alert
 // Removed lab training alert 1-9-2026, will re-enable whne the new training link is avaialable.
-//    { // Lab training Alert
 //      labTraining
 //        ? <Alert variant="filled" severity="info" onClose={handleDismissLabTraining} sx={{ borderRadius: 0 }}>
 //          All Makerspace users must complete the <a href="https://www.youtube.com/watch?v=XfELJU1mRMg">Shop Safety training course</a> before using any equipment.
