3256ede6585e7176c288f5afaba9ce9fe1852d46
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-01-16 09:43:13 -0500

    Created migration for tickets

---
+++ server/src/db/migrations/20260116142103_maintenance-tickets.ts
@@ -0,0 +1,29 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("MaintenanceTags");
+  await knex.schema.dropTableIfExists("MaintenanceLogs");
+  await knex.schema.dropTableIfExists("ResolutionLogs");
+
+  await knex.schema.createTable("MaintenanceTickets", (t) => {
+    t.increments("id").primary();
+    t.enu("severity", ["HIGH", "MEDIUM", "LOW"]).notNullable();
+    t.enu("type", ["AUTOMATIC", "REPORTED"]).notNullable();
+    t.integer("instanceID").references("id").inTable("EquipmentInstances").notNullable()
+      .onUpdate("CASCADE").onDelete("CASCADE");
+    t.integer("userID").references("id").inTable("Users").notNullable()
+      .onUpdate("CASCADE").onDelete("CASCADE");
+    t.string("description").notNullable().defaultTo("");
+    t.string("imageUrl").nullable().defaultTo(null);
+    t.boolean("closed").notNullable().defaultTo(false);
+    t.datetime("dateCreated").notNullable();
+    t.datetime("dateClosed").notNullable();
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("MaintenanceTickets");
+}
+
