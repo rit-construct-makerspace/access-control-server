ad3db67a806d86555760905fbbd35da0881abcd7
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-01-16 15:57:06 -0500

    mvp pagination setup

--- client/src/pages/makerspace_page/maintenance_pages/MaintenancePage.tsx
+++ client/src/pages/makerspace_page/maintenance_pages/MaintenancePage.tsx
@@ -5,6 +5,16 @@ import { useQuery } from "@apollo/client";
 import RequestWrapper2 from "../../../common/RequestWrapper2";
 import { DataGrid, GridRowsProp, GridColDef, GridFilterModel, GridPaginationModel, GridSortModel } from "@mui/x-data-grid";
 import { useState } from "react";
+import { MaintenanceTicket, PAGINATED_MAINTENANCE_TICKETS } from "../../../queries/maintenanceTicketQueries";
+
+const formatter = new Intl.DateTimeFormat("en-US", {
+  timeZone: "America/New_York",
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "2-digit"
+});
 
 export default function MaintenancePage() {
   const { makerspaceID } = useParams<{ makerspaceID: string }>();
@@ -14,6 +24,15 @@ export default function MaintenancePage() {
   const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({ page: 0, pageSize: 100, });
   const [filterModel, setFilterModel] = useState<GridFilterModel>();
   const [sortModel, setSortModel] = useState<GridSortModel>();
+
+  const getMaintenanceTickets = useQuery(PAGINATED_MAINTENANCE_TICKETS, {
+    variables: {
+      pagination: {
+        page: paginationModel.page,
+        pageSize: paginationModel.pageSize
+      }
+    }
+  });
 
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID" },
@@ -46,21 +65,42 @@ export default function MaintenancePage() {
       const makerspace: FullMakerspace = data.makerspaceByID;
 
       return (
-        <Stack padding={"15px"} spacing={2}>
-          <Typography>{`Maintenance Items for ${makerspace.name}`}</Typography>
-          <DataGrid
-            columns={columns}
-            paginationMode="server"
-            paginationModel={paginationModel}
-            onPaginationModelChange={handlePaginationModelChange}
-            filterMode="server"
-            filterModel={filterModel}
-            onFilterModelChange={handleFilterModelChange}
-            sortingMode="server"
-            sortModel={sortModel}
-            onSortModelChange={handleSortModelChange}
-          />
-        </Stack>
+        <RequestWrapper2 result={getMaintenanceTickets} render={(data) => {
+
+          const tickets: MaintenanceTicket[] = data.paginatedMaintenanceTickets;
+
+          const rows: GridRowsProp = tickets.map((ticket) => (
+            {
+              id: ticket.id,
+              equipment: ticket.instance.equipment.name,
+              instance: ticket.instance.name,
+              type: ticket.type,
+              status: ticket.status,
+              creator: ticket.creator.ritUsername,
+              created: formatter.format(new Date(ticket.dateCreated))
+            }
+          ))
+
+          return (
+            <Stack padding={"15px"} spacing={2}>
+              <Typography>{`Maintenance Items for ${makerspace.name}`}</Typography>
+              <DataGrid
+                columns={columns}
+                rows={rows}
+                paginationMode="server"
+                paginationModel={paginationModel}
+                onPaginationModelChange={handlePaginationModelChange}
+                filterMode="server"
+                filterModel={filterModel}
+                onFilterModelChange={handleFilterModelChange}
+                sortingMode="server"
+                sortModel={sortModel}
+                onSortModelChange={handleSortModelChange}
+                rowCount={-1}
+              />
+            </Stack>
+          );
+        }} />
       );
     }} />
   );
--- client/src/queries/maintenanceTicketQueries.ts
+++ client/src/queries/maintenanceTicketQueries.ts
@@ -1,25 +1,59 @@
 import { gql } from "@apollo/client";
+import { CurrentUser } from "../common/CurrentUserProvider";
+import { EquipmentInstance } from "./equipmentInstanceQueries";
+
+export enum MaintenanceTicketType {
+  "AUTOMATIC",
+  "REPORTED"
+}
+
+export enum MaintenanceTicketSeverity {
+  "HIGH",
+  "MEDIUM",
+  "LOW"
+}
+
+export enum MaintenanceTicketStatus {
+  "TODO",
+  "IN_PROGRESS",
+  "CLOSED"
+}
+
+export interface MaintenanceTicket {
+  id: number,
+  type: MaintenanceTicketType,
+  severity: MaintenanceTicketSeverity,
+  status: MaintenanceTicketStatus,
+  description: string,
+  imageUrl: string | null,
+  dateCreated: string,
+  dateClosed: string | null,
+  creator: CurrentUser,
+  instance: EquipmentInstance
+}
 
 export const PAGINATED_MAINTENANCE_TICKETS = gql`
   query PaginatedMaintenanceTickets($pagination: Pagination) {
-    id
-    type
-    severity
-    status
-    description
-    imageUrl
-    dateCreated
-    dateClosed
-    creator {
+    paginatedMaintenanceTickets(pagination: $pagination) {
       id
-      ritUsername
-    }
-    instance {
-      id
-      name
-      equipment {
+      type
+      severity
+      status
+      description
+      imageUrl
+      dateCreated
+      dateClosed
+      creator {
+        id
+        ritUsername
+      }
+      instance {
         id
         name
+        equipment {
+          id
+          name
+        }
       }
     }
   }
