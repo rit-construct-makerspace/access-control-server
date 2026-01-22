3c6db868cbc6ab33f1d4db358825b04c4830638e
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-01-22 14:21:07 -0500

    Removed date picker package

---
+++ client/src/pages/makerspace_page/maintenance_pages/NewIntervalTicketModal.tsx
@@ -0,0 +1,207 @@
+import { Autocomplete, Button, IconButton, Stack, TextField, Typography } from "@mui/material";
+import PrettyModal from "../../../common/PrettyModal";
+import CloseIcon from '@mui/icons-material/Close';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import Equipment from "../../../types/Equipment";
+import { FullMakerspace } from "../../../queries/makerspaceQueries";
+import { useMemo, useState } from "react";
+import { EquipmentInstance, GET_EQUIPMENT_INSTANCES } from "../../../queries/equipmentInstanceQueries";
+import { useMutation, useQuery } from "@apollo/client";
+import { CREATE_MAINTENANCE_TICKET, MaintenanceTicketSeverity } from "../../../queries/maintenanceTicketQueries";
+import { useCurrentUser } from "../../../common/CurrentUserProvider";
+import { toast } from "react-toastify";
+import FileUploadButton from "../../../common/FileUploadButton";
+import styled from "styled-components";
+import { makeCDNLink } from "../../../common/ImageFinder";
+
+const StyledImg = styled.img`
+  padding: 5px
+`;
+
+interface NewTicketModalProps {
+  open: boolean,
+  onClose: () => void,
+  makerspace?: FullMakerspace
+}
+
+export default function NewIntervalTicketModal(props: NewTicketModalProps) {
+  const user = useCurrentUser();
+
+  const [equipment, setEquipment] = useState<Equipment>();
+  const [instance, setInstance] = useState<EquipmentInstance>();
+  const [description, setDescription] = useState("");
+  const [severity, setSeverity] = useState<MaintenanceTicketSeverity>();
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const equipmentInstancesResult = useQuery(GET_EQUIPMENT_INSTANCES, { variables: { equipmentID: equipment?.id ?? -1 } });
+  const [createTicket] = useMutation(CREATE_MAINTENANCE_TICKET, { refetchQueries: ["PaginatedMaintenanceTickets", "MaintenanceTickets"] });
+
+  const EMPTY_ARRAY: EquipmentInstance[] = [];
+  const instances: EquipmentInstance[] = equipmentInstancesResult.data?.equipmentInstances ?? EMPTY_ARRAY;
+
+  const reportedInstance: EquipmentInstance | undefined = useMemo(() => {
+    if (instances.length === 1) {
+      return instances[0];
+    } else {
+      return instance;
+    }
+  }, [instances, instance]);
+
+
+  const makerspace_equipments_2 = props.makerspace?.rooms.map((room) => (room.equipment))
+  const makerspace_equipments = makerspace_equipments_2?.flat(1);
+
+  function handleClose() {
+    setEquipment(undefined);
+    setInstance(undefined);
+    setDescription("");
+    setSeverity(undefined);
+    setImageUrl(undefined);
+
+    props.onClose();
+  }
+
+  async function handleCreateTicket() {
+    if (!(equipment && reportedInstance && (severity !== undefined))) {
+      toast.error("A required field is empty!");
+      return;
+    }
+    try {
+      await createTicket({
+        variables: {
+          severity: severity,
+          instanceID: Number(reportedInstance.id),
+          userID: Number(user.id),
+          description: description,
+          imageUrl: imageUrl
+        }
+      })
+    } catch (e) {
+      toast.error("Failed to create ticket: " + e);
+      handleClose();
+      return;
+    }
+
+    toast.success("Created ticket!");
+    handleClose();
+    return;
+  }
+
+  return (
+    <PrettyModal open={props.open} onClose={handleClose} width={"620px"}>
+      <Stack spacing={2} padding={"10px"}>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography variant="h6">Create a Time-Based Maintenance Ticket</Typography>
+          <IconButton onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </Stack>
+        <Autocomplete
+          renderInput={
+            (params) => (
+              <TextField
+                {...params}
+                label="Equipment"
+                placeholder="Select Equipment..."
+                required
+              />
+            )
+          }
+          options={makerspace_equipments ?? []}
+          getOptionLabel={(option) => (option?.name ?? "You shouldn't see this")}
+          value={equipment}
+          onChange={(event, newValue) => setEquipment(newValue ?? undefined)}
+        />
+        {
+          equipment
+            ? <Autocomplete
+              key={instances.length === 1 ? "auto-selected" : "manual-select"}
+              renderInput={
+                (params) => (
+                  <TextField
+                    {...params}
+                    label="Instance"
+                    placeholder="Select Instance..."
+                    required
+                  />
+                )
+              }
+              options={instances}
+              getOptionLabel={(option) => (option?.name ?? "You shouldn't see this")}
+              value={reportedInstance}
+              onChange={(event, newValue) => setInstance(newValue ?? undefined)}
+              readOnly={instances.length === 1}
+            />
+            : <Autocomplete
+              renderInput={
+                (params) => (
+                  <TextField
+                    {...params}
+                    label="Instance"
+                    placeholder="Select Instance..."
+                    required
+                  />
+                )
+              }
+              options={[]}
+              disabled
+            />
+        }
+        <Autocomplete
+          renderInput={
+            (params) => (
+              <TextField
+                {...params}
+                label="Severity"
+                placeholder="Select Severity..."
+                required
+              />
+            )
+          }
+          options={[MaintenanceTicketSeverity.HIGH, MaintenanceTicketSeverity.MEDIUM, MaintenanceTicketSeverity.LOW]}
+          value={severity}
+          onChange={(event, newValue) => setSeverity(newValue ?? undefined)}
+        />
+        <Stack direction={"row"} justifyContent={"space-between"}>
+
+        </Stack>
+        <TextField
+          label={"Description"}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          multiline
+          minRows={3}
+        />
+        {
+          imageUrl
+            ? <StyledImg src={makeCDNLink(imageUrl, "user-uploads/")} />
+            : null
+        }
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Button
+            color="error"
+            variant="contained"
+            startIcon={<CloseIcon />}
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+          <FileUploadButton
+            color="info"
+            variant="contained"
+            text="Upload Image"
+            onUpload={(name: string) => setImageUrl(name)}
+          />
+          <Button
+            color="success"
+            variant="contained"
+            startIcon={<NoteAddIcon />}
+            onClick={handleCreateTicket}
+          >
+            Create Ticket
+          </Button>
+        </Stack>
+      </Stack>
+    </PrettyModal >
+  );
+}
