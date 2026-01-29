9bc8286f7fbf8fc9b6a79213646e20c528969b96
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-01-29 14:46:40 -0500

    no delete button for live trainings

--- client/src/pages/lab_management/training_modules/TrainingModuleRow.tsx
+++ client/src/pages/lab_management/training_modules/TrainingModuleRow.tsx
@@ -1,9 +1,14 @@
-import { CardActionArea, Stack, Typography } from "@mui/material";
+import { CardActionArea, IconButton, Stack, Typography } from "@mui/material";
 import { useNavigate, useParams } from "react-router-dom";
 import PublishTrainingModuleButton from "./PublishTrainingModuleButton";
 import ArchiveTrainingModuleButton from "./ArchiveTrainingModuleButton";
-import DeleteTrainingModuleButton from "./DeleteTrainingModuleButton";
 import { TrainingModule } from "../../../common/TrainingModuleUtils";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { DELETE_MODULE } from "../../../queries/trainingQueries";
+import { useMutation } from "@apollo/client";
+import { toast } from "react-toastify";
+import { useCurrentUser } from "../../../common/CurrentUserProvider";
+import { isManagerFor } from "../../../common/PrivilegeUtils";
 
 interface TrainingModuleProps {
   module: TrainingModule;
@@ -13,6 +18,25 @@ export default function TrainingModuleRow({ module }: TrainingModuleProps) {
   const { makerspaceID } = useParams<{ makerspaceID: string }>();
   const navigate = useNavigate();
   const url = `/makerspace/${makerspaceID}/training/${module.id}`;
+  const user = useCurrentUser();
+
+  const [deleteTrainingModule] = useMutation(DELETE_MODULE, {
+    variables: { id: module.id },
+    refetchQueries: ["GetTrainingModules"]
+  });
+
+  async function handleDeleteModule() {
+    if (!window.confirm("Are you sure you want to delete this training module?")) {
+      return;
+    }
+
+    try {
+      await deleteTrainingModule();
+    } catch (error) {
+      toast.error(`Failed to delete training module: ${error}`);
+      return;
+    }
+  }
 
   return (
 
@@ -38,7 +62,17 @@ export default function TrainingModuleRow({ module }: TrainingModuleProps) {
           ? <PublishTrainingModuleButton moduleID={module.id} appearance="icon-only" />
           : <ArchiveTrainingModuleButton moduleID={module.id} appearance="icon-only" />
       }
-      <DeleteTrainingModuleButton moduleID={module.id} appearance="icon-only" />
+      {
+        module.archived
+          ? <IconButton
+            color="error"
+            onClick={handleDeleteModule}
+            disabled={!isManagerFor(user, Number(makerspaceID ?? -1))}
+          >
+            <DeleteIcon />
+          </IconButton>
+          : null
+      }
     </Stack>
   );
 }
