d90a8c7d70b75ae32f93234dffbf01ad096c85a9
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-02-03 10:07:55 -0500

    Added endpoint for returning cards

--- server/src/api/devices/cards/cardapi.ts
+++ server/src/api/devices/cards/cardapi.ts
@@ -3,6 +3,7 @@ import * as Atrium from "../../../integrations/atrium-integration/atrium.js";
 import { CurrencySource } from "../../../integrations/currency/types.js";
 import * as UserRepo from "../../../repositories/Users/UserRepository.js";
 import * as TempCardRepo from "../../../repositories/Users/TempCardRepository.js";
+import { error } from "console";
 
 export function registerEndpoints(app: express.Application) {
 
@@ -33,10 +34,10 @@ export function registerEndpoints(app: express.Application) {
     try {
       const cardUsers = await TempCardRepo.getUserFromTempCardTag(cardTagID);
       if (cardUsers !== undefined) {
-        return res.status(409).json({ error: "A user has already been loaned the given cardTagID" });
+        return res.status(409).json({ error: "A user has already been loaned the given cardTagID" }).send();
       }
     } catch (e) {
-      return res.status(500).json({ error: "cardTagID already has multiple users" });
+      return res.status(500).json({ error: "cardTagID already has multiple users" }).send();
     }
 
     try {
@@ -47,17 +48,26 @@ export function registerEndpoints(app: express.Application) {
     }
   });
 
-  app.post("/api/devices/cards/return", async function (req, res) {
+  app.post("/api/devices/cards/disassociate", async function (req, res) {
     if (req.body.cardTagID === undefined) { return res.status(400).json({ error: "Missing cardTagID" }).send(); }
     const cardTagID: string = req.body.cardTagID;
 
     try {
       const cardUsers = await TempCardRepo.getUserFromTempCardTag(cardTagID);
       if (cardUsers === undefined) {
-        return res.status(409).json({ error: "the given cardTagID has not been loaned" });
+        return res.status(409).json({ error: "the given cardTagID has not been loaned" }).send();
       }
     } catch (e) {
-      return res.status(500).json({ error: "cardTagID already has multiple users" });
+      return res.status(500).json({ error: "cardTagID already has multiple users" }).send();
+    }
+
+    const returnedCards = await TempCardRepo.ReturnCard(cardTagID);
+    if (returnedCards.length === 0) {
+      return res.status(500).json({ error: "Failed to return card" }).send();
+    } else if (returnedCards.length === 1) {
+      return res.sendStatus(200);
+    } else {
+      return res.status(200).json({ error: "Card was returned multiple times when it should have been returned once" }).send();
     }
   })
 }
