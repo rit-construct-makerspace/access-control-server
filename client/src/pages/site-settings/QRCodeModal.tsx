51cc8c706abfac2fd4be456741ef52cd11df3583
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-02-04 17:26:04 -0500

    copiable qr codes

--- client/src/pages/site-settings/QRCodeModal.tsx
+++ client/src/pages/site-settings/QRCodeModal.tsx
@@ -1,6 +1,7 @@
 import QRCode from "react-qr-code";
 import PrettyModal from "../../common/PrettyModal";
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
+import { useRef } from "react";
 
 
 export default function QRCodeModal(props: {
@@ -9,11 +10,53 @@ export default function QRCodeModal(props: {
   link: string
 }) {
 
+  const svgRef = useRef(null);
+
+  function copyQRCode() {
+    const svg = svgRef.current;
+    if (svg === null) { return; }
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+
+    const svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    const url = URL.createObjectURL(svgBlob);
+
+    img.onload = () => {
+      // Set canvas dimensions to match SVG
+      canvas.width = img.width;
+      canvas.height = img.height;
+      if (ctx === null) { return };
+      ctx.drawImage(img, 0, 0);
+
+      // Copy to clipboard as a PNG
+      canvas.toBlob((blob) => {
+        if (blob === null) { return; }
+        const item = new ClipboardItem({ "image/png": blob });
+        navigator.clipboard.write([item]);
+      });
+
+      URL.revokeObjectURL(url);
+    };
+
+    img.src = url;
+  }
+
   return (
     <PrettyModal open={props.open} onClose={props.onClose} width={"max-content"}>
       <Stack spacing={2} width={"max-content"} alignItems={"center"}>
-        <Typography color="primary" variant="h4">{`/${props.link}`}</Typography>
-        <QRCode value={`https://make.rit.edu/${props.link}`} size={500} />
+        <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} width={"100%"}>
+          <Typography color="primary" variant="h4">{`/${props.link}`}</Typography>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={copyQRCode}
+          >
+            Copy
+          </Button>
+        </Stack>
+        <QRCode value={`https://make.rit.edu/${props.link}`} size={500} ref={svgRef} />
       </Stack>
     </PrettyModal>
   );
