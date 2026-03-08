1a3222d45986b2c8fa24d4d5335cc61b961bb3a2
cowsed <richie.sommers@outlook.com>
2026-03-08 15:59:58 -0400

    remove debug call in transaction repo for getting charges

--- server/src/repositories/Currency/TransactionRepository.ts
+++ server/src/repositories/Currency/TransactionRepository.ts
@@ -102,7 +102,7 @@ export async function getLastChargesForTransactionById(transactionId: number): P
         )
         .leftJoin("CurrencyLedger as cl", "cl.transactionEntryId", "te.id")
         .where("te.transactionID", "=", transactionId)
-        .orderBy("te.dateTime", "desc").debug(true) as Row[];
+        .orderBy("te.dateTime", "desc") as Row[];
 
     const rowsNotIncludingEmptyEntries = rows.filter(r => r.CLID !== null);
     if (rowsNotIncludingEmptyEntries.length == 0) {
