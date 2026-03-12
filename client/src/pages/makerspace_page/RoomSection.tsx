0709f7be1c5bbf23cdbbd49fc7efe5ba94d75a6f
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-03-12 15:50:28 -0400

    Training Reminders Cleanup (#1367)

--- client/src/pages/makerspace_page/RoomSection.tsx
+++ client/src/pages/makerspace_page/RoomSection.tsx
@@ -1,11 +1,9 @@
-import { Alert, Divider, Grid, Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import Room from "../../types/Room";
 import Equipment from "../../types/Equipment";
 import EquipmentCard from "../../common/EquipmentCard";
-import { useCurrentUser } from "../../common/CurrentUserProvider";
-import { ModuleStatus, moduleStatusMapper, TrainingModule } from "../../common/TrainingModuleUtils";
+import { TrainingModule } from "../../common/TrainingModuleUtils";
 import { useIsMobile } from "../../common/IsMobileProvider";
-import ModuleStatusRow from "../../common/ModuleStatusRow";
 
 interface RoomSectionProps {
   room: Room;
@@ -21,7 +19,6 @@ interface RoomSectionProps {
 }
 
 export default function RoomSection(props: RoomSectionProps) {
-  const currentUser = useCurrentUser();
   const isMobile = useIsMobile();
   const roomEquipment = props.room.equipment;
 
@@ -30,8 +27,6 @@ export default function RoomSection(props: RoomSectionProps) {
   const archivedEquipment = sortedEquipment.filter((equipment: Equipment) => equipment.archived);
   const liveEquipment = sortedEquipment.filter((equipment: Equipment) => !equipment.archived);
 
-  const roomTrainings = props.room.trainingModules.map(moduleStatusMapper(currentUser.passedModules, currentUser.trainingHolds));
-
   if (liveEquipment.length < 1) {
     return null;
   }
@@ -39,27 +34,6 @@ export default function RoomSection(props: RoomSectionProps) {
   return (
     <Stack padding={"10px 0"} spacing={1}>
       <Typography variant="h4" pl={"10px"}>{props.room.name}</Typography>
-      {
-        roomTrainings.length > 0 &&
-        <Stack direction={"column"} alignItems={isMobile ? "center" : "flex-start"} pl={"10px"} spacing={1}>
-          <Stack direction={isMobile ? "column" : "row"} spacing={2} alignItems={"center"}>
-            <Typography variant="h6">Area Trainings</Typography>
-            {
-              roomTrainings.some((ms) => (ms.status !== "Passed" && ms.status !== "Expiring Soon"))
-                ? <Alert severity="error">You must pass the area trainings before you can use equipment in the area!</Alert>
-                : null
-            }
-          </Stack>
-          <Stack direction={isMobile ? "column" : "row"} spacing={1} alignItems={"center"}>
-            {
-              roomTrainings.length > 0 && roomTrainings.map((ms: ModuleStatus) => (
-                <ModuleStatusRow ms={ms} />
-              ))
-            }
-          </Stack>
-          <Divider orientation="horizontal" variant="fullWidth" flexItem />
-        </Stack>
-      }
       <Grid container spacing={isMobile ? 2 : 3} justifyContent="center">
         {
           liveEquipment.map((equipment: Equipment) => (
