0948682c5718db188169847a14ae14271e8e6de2
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-03-25 16:35:49 -0400

    fixed syntax in schema

--- server/src/schemas/deviceSchema.ts
+++ server/src/schemas/deviceSchema.ts
@@ -84,7 +84,7 @@ export const DeviceTypeDefs = gql`
 
   extend type Query {
     getAccessControllerByID(accessControllerID: Int!): AccessController
-    getUnpairedAccessControllers(): [AccessController]
+    getUnpairedAccessControllers: [AccessController]
   }
 
   input CoreFlagInput {
