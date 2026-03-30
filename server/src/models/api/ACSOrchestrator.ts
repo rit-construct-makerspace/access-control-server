bda06753876ab1cfd9c4c8659bc60b0d4dd365a1
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-03-30 10:01:25 -0400

    Added try-catch Handlers

--- server/src/models/api/ACSOrchestrator.ts
+++ server/src/models/api/ACSOrchestrator.ts
@@ -20,131 +20,158 @@ export class ACSOrchestrator {
   }
 
   public static async handleCoreStatusReport(deviceID: number, statusReport: CoreStatusReport) {
+    try {
+      const core = await CoreRepo.getCoreByDeviceID(deviceID);
+      if (core === undefined) { return; }
 
-    const core = await CoreRepo.getCoreByDeviceID(deviceID);
-    if (core === undefined) { return; }
-
-    await core.statusUpdate(statusReport.currentCardTag);
-    statusReport.channels.forEach(async (channel) => await core.updateControllerState(channel.channelID, channel.state));
+      await core.statusUpdate(statusReport.currentCardTag);
+      statusReport.channels.forEach(async (channel) => await core.updateControllerState(channel.channelID, channel.state));
+    } catch (e) {
+      await DeviceLogRepo.createDeviceLog(deviceID, DeviceLogSeverity.MEDIUM, { type: "core-status-error", error: e });
+    }
   }
 
   public static async handleCoreStateChangeReport(deviceID: number, stateChangeReport: CoreStateChangeReport) {
-    const core = await CoreRepo.getCoreByDeviceID(deviceID);
-    if (core === undefined) { return; }
+    try {
+      const core = await CoreRepo.getCoreByDeviceID(deviceID);
+      if (core === undefined) { return; }
 
-    const oldCardTag = core.currentCardTag;
+      const oldCardTag = core.currentCardTag;
 
-    await core.statusUpdate(stateChangeReport.currentCardTag);
+      await core.statusUpdate(stateChangeReport.currentCardTag);
 
-    stateChangeReport.channels.forEach(async (channel) => {
-      if (channel.fromState === AccessControllerState.UNLOCKED) {
-        // Leaving UNLOCKED, register an end of session message
-        (await ACRepo.getAccessControllersByDeviceAndChannelID(deviceID, channel.channelID))?.endSession(oldCardTag ?? "");
-      } else if (channel.toState === AccessControllerState.UNLOCKED) {
-        // TODO: Register start of session
-      }
-      await core.updateControllerState(channel.channelID, channel.toState);
-    })
+      stateChangeReport.channels.forEach(async (channel) => {
+        if (channel.fromState === AccessControllerState.UNLOCKED) {
+          // Leaving UNLOCKED, register an end of session message
+          (await ACRepo.getAccessControllersByDeviceAndChannelID(deviceID, channel.channelID))?.endSession(oldCardTag ?? "");
+        } else if (channel.toState === AccessControllerState.UNLOCKED) {
+          // TODO: Register start of session
+        }
+        await core.updateControllerState(channel.channelID, channel.toState);
+      })
+    } catch (e) {
+      await DeviceLogRepo.createDeviceLog(deviceID, DeviceLogSeverity.MEDIUM, { type: "core-state-change-report-error", error: e });
+    }
   }
 
   public static async handleCoreLogRequest(deviceID: number, logRequest: CoreLogRequest) {
-    const core = await CoreRepo.getCoreByDeviceID(deviceID);
-    if (core === undefined) { return; }
+    try {
+      const core = await CoreRepo.getCoreByDeviceID(deviceID);
+      if (core === undefined) { return; }
 
-    if (logRequest.auditLog) {
-      AuditLogRepo.createAuditLog(
-        `Message from {device}: ${logRequest.message}`,
-        logRequest.category,
-        core.makerspaceID,
-        { id: core.deviceID, label: core.name }
-      )
-    } else {
-      DeviceLogRepo.createDeviceLog(
-        core.deviceID,
-        DeviceLogSeverity.LOW,
-        { type: "message", message: logRequest.message }
-      )
+      if (logRequest.auditLog) {
+        AuditLogRepo.createAuditLog(
+          `Message from {device}: ${logRequest.message}`,
+          logRequest.category,
+          core.makerspaceID,
+          { id: core.deviceID, label: core.name }
+        )
+      } else {
+        DeviceLogRepo.createDeviceLog(
+          core.deviceID,
+          DeviceLogSeverity.LOW,
+          { type: "message", message: logRequest.message }
+        )
+      }
+    } catch (e) {
+      await DeviceLogRepo.createDeviceLog(deviceID, DeviceLogSeverity.MEDIUM, { type: "core-log-error", error: e });
     }
   }
 
   public static async handleCoreAuthToRequest(deviceID: number, authToRequest: CoreAuthToRequest) {
-    const core = await CoreRepo.getCoreByDeviceID(deviceID);
-    const user = await UserRepo.getUserByCardTagID(authToRequest.cardTagID);
+    try {
+      const core = await CoreRepo.getCoreByDeviceID(deviceID);
+      const user = await UserRepo.getUserByCardTagID(authToRequest.cardTagID);
 
-    if (core === undefined || user === undefined) {
-      AuditLogRepo.createAuditLog(
-        `{user} failed to activate {device}`,
-        "auth",
-        core?.makerspaceID,
-        { id: user?.id ?? -1, label: user ? `${user.firstName} ${user.lastName}` : "Unknown User" },
-        { id: deviceID, label: core ? core.name : "unkown device" }
-      );
+      if (core === undefined || user === undefined) {
+        AuditLogRepo.createAuditLog(
+          `{user} failed to activate {device}`,
+          "auth",
+          core?.makerspaceID,
+          { id: user?.id ?? -1, label: user ? `${user.firstName} ${user.lastName}` : "Unknown User" },
+          { id: deviceID, label: core ? core.name : "unkown device" }
+        );
 
-      if (core !== undefined) {
+        if (core !== undefined) {
+          const controllers = await core.getAccessControllers();
+          ACSOrchestrator.getDeviceController(deviceID)?.sendCoreAuthToResponse(core, {
+            channels: controllers.map((controller) => ({
+              channelID: controller.channelID,
+              state: authToRequest.state,
+              approved: false,
+              reason: AccessAttemptReason.UNKNOWN_USER
+            })),
+            cardTagID: authToRequest.cardTagID
+          })
+        }
+        return;
+      }
+
+      const welcomeSpace = await core.getWelcomeMakerspace();
+      if (welcomeSpace !== undefined) {
+        // This is a welcome reader, welcome and return approved rather than chcking access
+        await welcomeSpace.welcome(user.id);
+
         const controllers = await core.getAccessControllers();
+
         ACSOrchestrator.getDeviceController(deviceID)?.sendCoreAuthToResponse(core, {
           channels: controllers.map((controller) => ({
             channelID: controller.channelID,
-            state: authToRequest.state,
-            approved: false,
-            reason: AccessAttemptReason.UNKNOWN_USER
+            state: AccessControllerState.UNLOCKED,
+            approved: true,
+            reason: AccessAttemptReason.WELCOME
           })),
           cardTagID: authToRequest.cardTagID
         })
+        return;
       }
-      return;
-    }
 
-    const welcomeSpace = await core.getWelcomeMakerspace();
-    if (welcomeSpace !== undefined) {
-      // This is a welcome reader, welcome and return approved rather than chcking access
-      await welcomeSpace.welcome(user.id);
-
-      const controllers = await core.getAccessControllers();
+      const attemptResult = await core.authTo(user.id, authToRequest.state, true);
 
       ACSOrchestrator.getDeviceController(deviceID)?.sendCoreAuthToResponse(core, {
-        channels: controllers.map((controller) => ({
-          channelID: controller.channelID,
-          state: AccessControllerState.UNLOCKED,
-          approved: true,
-          reason: AccessAttemptReason.WELCOME
-        })),
+        channels: attemptResult,
         cardTagID: authToRequest.cardTagID
       })
-      return;
+    } catch (e) {
+      await DeviceLogRepo.createDeviceLog(deviceID, DeviceLogSeverity.MEDIUM, { type: "core-authTo-error", error: e });
     }
-
-    const attemptResult = await core.authTo(user.id, authToRequest.state, true);
-
-    ACSOrchestrator.getDeviceController(deviceID)?.sendCoreAuthToResponse(core, {
-      channels: attemptResult,
-      cardTagID: authToRequest.cardTagID
-    })
   }
 
   public static async handleCoreConfigReport(deviceID: number, configReport: CoreConfigReport) {
+    try {
 
+    } catch (e) {
+      await DeviceLogRepo.createDeviceLog(deviceID, DeviceLogSeverity.MEDIUM, { type: "core-config-report-error", error: e });
+    }
   }
 
   public static async handleCoreInfoRequest(deviceID: number, infoRequest: CoreInfoRequest) {
-    const core = await CoreRepo.getCoreByDeviceID(deviceID);
-    if (core === undefined) { return; }
+    try {
+      const core = await CoreRepo.getCoreByDeviceID(deviceID);
+      if (core === undefined) { return; }
 
-    const response: ServerInfoResponse = {
-      time: infoRequest.fields.includes(CoreInfoOptions.TIME)
-        ? (new Date).getTime() : undefined,
-      state: infoRequest.fields.includes(CoreInfoOptions.STATE)
-        ? (await core.getAccessControllers()).map((controller) => ({ id: controller.channelID, state: controller.state })) : undefined,
-      hmi: undefined
+      const response: ServerInfoResponse = {
+        time: infoRequest.fields.includes(CoreInfoOptions.TIME)
+          ? (new Date).getTime() : undefined,
+        state: infoRequest.fields.includes(CoreInfoOptions.STATE)
+          ? (await core.getAccessControllers()).map((controller) => ({ id: controller.channelID, state: controller.state })) : undefined,
+        hmi: undefined
+      }
+
+      ACSOrchestrator.getDeviceController(core.deviceID)?.sendCoreInfoResponse(core, response);
+    } catch (e) {
+      await DeviceLogRepo.createDeviceLog(deviceID, DeviceLogSeverity.MEDIUM, { type: "core-info-request-error", error: e });
     }
-
-    ACSOrchestrator.getDeviceController(core.deviceID)?.sendCoreInfoResponse(core, response);
   }
 
   public static async handleSendCoreCommand(deviceID: number, command: ServerCommand) {
-    const core = await CoreRepo.getCoreByDeviceID(deviceID);
-    if (core === undefined) { return; }
+    try {
+      const core = await CoreRepo.getCoreByDeviceID(deviceID);
+      if (core === undefined) { return; }
 
-    ACSOrchestrator.getDeviceController(deviceID)?.sendCoreCommand(core, command);
+      ACSOrchestrator.getDeviceController(deviceID)?.sendCoreCommand(core, command);
+    } catch (e) {
+      await DeviceLogRepo.createDeviceLog(deviceID, DeviceLogSeverity.MEDIUM, { type: "send-core-command-error", error: e });
+    }
   }
 }
