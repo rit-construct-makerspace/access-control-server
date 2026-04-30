1172ecd8e73c95d9476de2afdf578255631003da
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-04-30 16:46:56 -0400

    Resolved warning in equipmentInstanceQueries

--- client/src/queries/equipmentInstanceQueries.ts
+++ client/src/queries/equipmentInstanceQueries.ts
@@ -1,5 +1,5 @@
 import { gql } from "@apollo/client";
-import { AccessController, AccessControllerState } from "./deviceQueries";
+import { AccessController } from "./deviceQueries";
 
 export enum InstanceStatus {
   UNDEPLOYED = "UNDEPLOYED",
