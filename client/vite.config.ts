a400a83818714b7e24c07848a535851310258185
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-04-30 11:12:10 -0400

    Fixed deps so vite can run in dev mode

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,31 +2,35 @@ import { defineConfig } from 'vite';
 import eslint from "vite-plugin-eslint2";
 import react from "@vitejs/plugin-react"
 
-export default defineConfig({
-  plugins: [
-    react(),
-    eslint({
-      dev: true,
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
 
-      build: true, // lint on build
-      cache: true, // cache lints
-      emitWarning: true,
-      emitError: true,
-      // Don't fail build on errors (bc the entire build would fail rn)
-      // if its 2026 and this is still here you should feel bad
-      emitErrorAsWarning: true,
+  return {
+    plugins: [
+      react(),
+      eslint({
+        dev: true,
 
-    }),
-  ],
-  ssr: {
-    noExternal: true,
-    external: ["react", "react-dom", "react-dom/server"],
-  },
-  base: "/app/",
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    port: 3001, // dont conflict with dev server which is on 3000
-  },
+        build: true, // lint on build
+        cache: true, // cache lints
+        emitWarning: true,
+        emitError: true,
+        // Don't fail build on errors (bc the entire build would fail rn)
+        // if its 2026 and this is still here you should feel bad
+        emitErrorAsWarning: true,
+
+      }),
+    ],
+    ssr: {
+      noExternal: isDev ? ["@mui/x-data-grid"] : true,
+      external: ["react", "react-dom", "react-dom/server"],
+    },
+    base: "/app/",
+    build: {
+      outDir: 'build',
+    },
+    server: {
+      port: 3001, // dont conflict with dev server which is on 3000
+    },
+  }
 });
