97f39410238c6d48159f377e5637469dfbcef923
ZZwatermelon <63483612+ZZwatermelon@users.noreply.github.com>
2026-05-04 15:38:17 -0400

    Made quiz content 80% wide instead of 100%

--- client/src/pages/maker/take_quiz/QuizTaker.tsx
+++ client/src/pages/maker/take_quiz/QuizTaker.tsx
@@ -161,7 +161,7 @@ export default function QuizTaker({ module }: QuizTakerProps) {
 
 
   return (
-    <Stack spacing={4} sx={styles.strongerBolds}>
+    <Stack spacing={4} sx={styles.strongerBolds} width={"80%"}>
       <title>{`${module.name} | ${makeTheme.title}`}</title>
       {module.isLocked && (
         <Card>
